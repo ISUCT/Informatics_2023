acc2ab98eb08276e9526415eecb43707b424d14e
Kozlov Egor Alexandroivich <PublicDevelMail@yandex.ru>
2023-09-20 21:25:23 +0300

    CodeWars: Even Or Odd improvenment was made.

--- CodeWarsExercises/Codewars/EvenOrOdd.cs
+++ CodeWarsExercises/Codewars/EvenOrOdd.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeWarsExercises.Codewars {
+﻿namespace CodeWarsExercises.Codewars {
     internal class EvenOrOdd : IExercise {
         public string Name => "Even or odd";
 
         public void Run() {
             // Note: 0 is even.
-            Console.WriteLine("Please, enter number:");
-            Console.WriteLine(ConsoleUtils.ReadInt() % 2 == 0 ? "Even" : "Odd");
-
+            Console.WriteLine(ConsoleUtils.ReadInt("Please, enter number: ") % 2 == 0 ? "Even" : "Odd");
         }
     }
 }
--- CodeWarsExercises/ConsoleUtils.cs
+++ CodeWarsExercises/ConsoleUtils.cs
@@ -3,8 +3,11 @@
 namespace CodeWarsExercises {
     internal static class ConsoleUtils {
 
-        public static int ReadInt() {
+        public static int ReadInt(string? message = null) {
             // TODO: Make this function exception-safe.
+            if (message is object)
+                Console.Write(message);
+
             return int.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
         }
     }
