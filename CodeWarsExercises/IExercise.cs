b3ac376e1efc30829f53639b4472dadca799c4ec
Kozlov Egor Alexandroivich <PublicDevelMail@yandex.ru>
2023-09-20 21:01:32 +0300

    Codewars: Mainloop for exercises written.

---
+++ CodeWarsExercises/IExercise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsExercises {
+    internal interface IExercise {
+        
+        public string Name { get; }
+        
+        void Run();
+    }
+}
---
+++ CodeWarsExercises/Program.cs
@@ -0,0 +1,49 @@
+﻿using CodeWarsExercises;
+using System.Globalization;
+
+using static System.Console;
+
+IExercise[] exercises = new IExercise[] {
+    // TODO: Fill with exercises.
+};
+
+while (true) {
+    WriteLine("Choose an exercise you wish to test:");
+
+    for (int index = 0; index < exercises.Length; ++index) {
+        WriteLine($"{index:00}. {exercises[index].Name}");
+    }
+
+    WriteLine();
+
+    while (true) {
+        Write("Please enter a number or 'e' symbol to exit: ");
+
+        string? userInput = ReadLine()?.Trim();
+
+        if (userInput is null || userInput == "e")
+            return; // user cut their input, so stop execution.
+
+        bool inputIsCorrect = int.TryParse(
+            userInput,
+            NumberStyles.Integer,
+            CultureInfo.InvariantCulture,
+            out int index
+            );
+
+        if (inputIsCorrect) {
+            if (index < 0 || index >= exercises.Length) {
+                WriteLine($"The index must lies between 0 and {exercises.Length}.");
+                WriteLine($"[0, {exercises.Length})");
+                continue;
+            }
+
+            Clear();
+            exercises[index].Run();
+
+            WriteLine("Hit Enter to continue...");
+            ReadLine();
+            Clear();
+        }
+    }
+}
