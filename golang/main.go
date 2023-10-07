f9b1cb868f4eab10b4fbb16d46b2a09e59a9683b
Vinogradov Anton <vino74974@gmail.com>
2023-10-08 00:22:51 +0300

    Added lab on calculating func`s

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Вингорадов Антон Алексеевич 1/278")
+	for i := 1.25; i <= 3.25; i = i + float64(0.4) {
+		res := func_resolution(i)
+		fmt.Println(res)
+	}
+	var numbers [5]float64 = [5]float64{1.84, 2.71, 3.81, 4.56, 5.62}
+	for i := 0; i <= len(numbers)-1; i++ {
+		res := func_resolution(numbers[i])
+		fmt.Println(res)
+	}
+}
+
+func func_resolution(x float64) float64 {
+	square := square(x)
+	diff := square - 2.5
+
+	mod := math.Abs(float64(diff))
+
+	root4 := math.Pow(float64(mod), root_nth_degree(4))
+
+	nat_log := math.Log(square)
+
+	root3 := math.Pow(float64(nat_log), root_nth_degree(3))
+
+	y := root4 + root3
+
+	return y
+}
+
+func square(x float64) float64 {
+	return x * x
+}
+func root_nth_degree(degree float64) float64 {
+	return float64(1) / float64(degree)
 }
