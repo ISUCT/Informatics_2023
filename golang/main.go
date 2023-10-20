1fcafdfc69ee7a646202340231a54f81c2da3f34
pvzprrrr <alenanaumovvva@mail.ru>
2023-10-20 17:50:56 +0300

    laba 3 variant 14

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
+	//Лабораторная 2
 	fmt.Println("Naumova Alena Alekseevna")
+
+	//Лабораторная 3 - вариант 14
+
+	//Решение под А
+	a := 7.2
+	b := 4.2
+	x__begin := 1.81
+	x__end := 5.31
+	x__delta := 0.7
+	fmt.Println("Решения под A:")
+	for x := x__begin; x <= x__end; x = x + x__delta {
+		var y1 = math.Sqrt((math.Abs(a - b*x)) / (math.Pow(math.Log10(x), 3)))
+		fmt.Println(y1)
+	}
+
+	//Решение под Б
+	fmt.Println("Решения под B:")
+	var arguments = [5]float64{2.4, 2.8, 3.9, 4.7, 3.16}
+	for n := 0; n < 5; n++ {
+		var y2 = math.Sqrt((math.Abs(a - b*arguments[n])) / (math.Pow(math.Log10(arguments[n]), 3)))
+		fmt.Println(y2)
+	}
+
 }
