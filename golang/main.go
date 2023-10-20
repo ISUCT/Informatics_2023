7bf582a37e349e12c9ee56393a249095d59c44ec
Иван Аксёнов <sk4inee@gmail.com>
2023-10-20 17:58:56 +0300

    Added lab 3 variant 1

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func ToRadians(deg float64) float64 {
+	return (deg) * (math.Pi / 180.0)
+}
+
+func Calculate(x float64, b float64) float64 {
+	return (1 + math.Pow(math.Sin(ToRadians(math.Pow(b, 3)+math.Pow(x, 3))), 2)) / math.Cbrt(math.Pow(b, 3)+math.Pow(x, 3))
+}
 
 func main() {
-	fmt.Println("Аксенов Иван Дмитриевич")
+	var b = 2.5
+	var begX = 1.28
+	var finX = 3.28
+	var detX = 0.4
+
+	for i := begX; i < finX; i += detX {
+		fmt.Println(Calculate(i, b))
+	}
+
+	var xList = [5]float64{1.1, 2.4, 3.6, 1.7, 3.9}
+
+	for i := 0; i < len(xList); i++ {
+		fmt.Println(Calculate(xList[i], b))
+	}
 }
