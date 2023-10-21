2a8f5681ad4617bea1cf5e8e2573a40eed5da96f
J0es1ick <mrjoeslick@gmail.com>
2023-10-21 06:18:14 +0300

    lab 3 var 12

--- golang/main.go
+++ golang/main.go
@@ -16,7 +16,7 @@ func main() {
 	x_begin := 1.2
 	x_end := 3.7
 	x_delta := 0.5
-	
+
 	fmt.Println("Решения задачи под А:")
 	for x := x_begin; x <= x_end; x += x_delta {
 		fmt.Println(result(a,x))
@@ -25,7 +25,6 @@ func main() {
 	fmt.Println("Решения задачи под В:")
 	var values = [5]float64{1.28, 1.36, 2.47, 3.68, 4.56}
 	for i := 0; i < 5; i++ {
-		x := values[i]
-		fmt.Println(result(a,x))
+		fmt.Println(result(a,values[i]))
 	}
 }
