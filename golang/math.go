312c3fb1f74b21364614533baef8d085f7bae674
MarkVRKS <143934528+MarkVRKS@users.noreply.github.com>
2023-10-23 17:41:23 +0300

    Update math.go
    Первая формула д.з.

--- golang/math.go
+++ golang/math.go
@@ -4,17 +4,14 @@ import (
 	"fmt"
 	"math"
 )
+
 func main() {
 	b := 2.5
 	for x := 1.28; x <= 3.28; x += 0.4 {
+		verh := (1 + (math.Sin(b*b*b+x*x*x) * math.Sin(b*b*b+x*x*x)))
+		niz := math.Cbrt(b*b*b + x*x*x)
+		result := verh / niz
 
+		fmt.Println("Результат первой формулы из таблицы - ", result)
 	}
-
-	skoba := (math.pow(b, 3) + math.pow(x, 3))
-	sinus := math.Sin(skoba)
-	verh := (1 + sinus*sinus)
-	niz := math.Cbrt(skoba)
-	itog := verh / niz
-
-	fmt.Println("Результат первой формулы из таблицы - ", itog)
 }
