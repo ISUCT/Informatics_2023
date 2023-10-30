269795230a795acc66f202dfc5015f35f7fd2dd7
rusafob <147656000+rusafob@users.noreply.github.com>
2023-10-30 14:46:15 +0300

    Update math.go

--- golang/math.go
+++ golang/math.go
@@ -1,1 +1,31 @@
+```go
+package main
 
+import (
+"fmt"
+"math"
+)
+
+func main() {
+startX := 0.11
+endX := 0.36
+step := 0.05
+
+x := startX
+for x <= endX {
+result := calculateFunction(x)
+fmt.Printf("x = %.2f, y = %.4f\n", x, result)
+x += step
+}
+}
+
+func calculateFunction(x float64) float64 {
+sinCubed := math.Pow(math.Sin(x), 3)
+cosCubed := math.Pow(math.Cos(x), 3)
+lnX := math.Log(x)
+
+y := (sinCubed + cosCubed) * lnX
+
+return y
+}
+```
