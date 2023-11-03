9b541d5bcc853cf4e2f1be3894c15070be46949f
Alexey Tselikov <koktera@gmail.com>
2023-11-03 11:00:26 +0300

    lab 3 (fixed root)

--- golang/main.go
+++ golang/main.go
@@ -7,7 +7,7 @@ import (
 
 func form(x float64) float64 {
 	a := 2.25
-	return (math.Pow(a, (math.Pow(x, 2)-1)) - math.Log10(math.Pow(x, 2)-1) + math.Pow((math.Pow(x, 2)-1), (1/3)))
+	return (math.Pow(a, (math.Pow(x, 2)-1)) - math.Log10(math.Pow(x, 2)-1) + math.Cbrt((math.Pow(x, 2) - 1)))
 }
 func main() {
 	fmt.Println("Целиков Алексей Александрович")
