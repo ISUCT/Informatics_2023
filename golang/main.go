9eeb1d753f4cedce1eb5ae064e04c13c556861c6
Alexey Pribylov <a_02_p111@mail.ru>
2023-11-03 21:26:08 +0300

    lab1 + lab2 +lab3

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+func formula(x float64) float64 {
+	return (math.Pow(math.Pow(math.Asin(x), 2)+math.Pow(math.Acos(x), 4), 3))
+}
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Прибылов Алексей Александрович")
+	fmt.Println("Задача A")
+	for i := 0.26; i <= 0.66; i += 0.08 {
+		fmt.Println("При x=", i, "y=", formula(i))
+	}
+	fmt.Println("Задача B")
+	var list [5]float64 = [5]float64{0.1, 0.35, 0.4, 0.55, 0.6}
+	for i := 0; i <= 4; i++ {
+		fmt.Println("При x=", list[i], "y=", formula(list[i]))
+	}
 }
