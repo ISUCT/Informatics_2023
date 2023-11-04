1e1f87a4c959350a86d298f85650b6673cc5f884
Vinogradov Anton <vino74974@gmail.com>
2023-11-04 16:44:31 +0300

    Change functions to a separate file

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -1,5 +1,49 @@
 package internal
 
+import (
+	"fmt"
+	"math"
+)
+
 func Summ(a, b int) int {
 	return a + b
+}
+
+func Print_answer() {
+	var numbers_cycle = []float64{}
+	var numbers_massive = []float64{}
+	for i := 1.25; i <= 3.25; i = i + float64(0.4) {
+		numbers_cycle = append(numbers_cycle, Func_resolution(i))
+	}
+	var numbers [5]float64 = [5]float64{1.84, 2.71, 3.81, 4.56, 5.62}
+	for i := 0; i <= len(numbers)-1; i++ {
+		numbers_massive = append(numbers_massive, Func_resolution(numbers[i]))
+	}
+	fmt.Println(numbers_massive)
+	fmt.Println(numbers_cycle)
+}
+
+func Func_resolution(x float64) float64 {
+	square := square(x)
+
+	diff := square - 2.5
+
+	mod := math.Abs(diff)
+
+	root4 := math.Pow(mod, root_nth_degree(4))
+
+	nat_log := math.Log(square)
+
+	root3 := math.Pow(nat_log, root_nth_degree(3))
+
+	y := root4 + root3
+
+	return y
+}
+
+func square(x float64) float64 {
+	return x * x
+}
+func root_nth_degree(degree float64) float64 {
+	return 1 / degree
 }
--- golang/main.go
+++ golang/main.go
@@ -2,42 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal"
 )
 
 func main() {
 	fmt.Println("Вингорадов Антон Алексеевич 1/278")
-	for i := 1.25; i <= 3.25; i = i + float64(0.4) {
-		res := func_resolution(i)
-		fmt.Println(res)
-	}
-	var numbers [5]float64 = [5]float64{1.84, 2.71, 3.81, 4.56, 5.62}
-	for i := 0; i <= len(numbers)-1; i++ {
-		res := func_resolution(numbers[i])
-		fmt.Println(res)
-	}
-}
-
-func func_resolution(x float64) float64 {
-	square := square(x)
-	diff := square - 2.5
-
-	mod := math.Abs(diff)
-
-	root4 := math.Pow(mod, root_nth_degree(4))
-
-	nat_log := math.Log(square)
-
-	root3 := math.Pow(nat_log, root_nth_degree(3))
-
-	y := root4 + root3
-
-	return y
-}
-
-func square(x float64) float64 {
-	return x * x
-}
-func root_nth_degree(degree float64) float64 {
-	return 1 / degree
+	internal.Print_answer()
 }
