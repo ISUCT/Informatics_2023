c8729438f743758caf69e80fe6c12bb16bcad1ff
boos1337 <schuher39@gmail.com>
2023-11-05 13:40:56 +0300

    lab 3

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func formula(x float64) float64 {
+	var result float64
+	result = math.Asin(math.Pow(x, 2)) + math.Asin(math.Pow(x, 3))
+	return result
+}
 
 func main() {
-	fmt.Println("Levin Roman Evgenevich")
+	fmt.Println("Задача А")
+	for i := 0.11; i <= 0.36; i += 0.05 {
+		fmt.Println("Если х = ", i, "то y = ", formula(i))
+	}
+	fmt.Println("Задача B")
+	var array [5]float64 = [5]float64{0.08, 0.26, 0.35, 0.41, 0.53}
+	for i := 0; i < 5; i++ {
+		fmt.Println("Если x = ", array[i], "то y = ", formula(array[i]))
+	}
 }
