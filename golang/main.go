10d5c8bb8d9a616f7be69a4dbadc1bf619e380f9
Olga Fedorova <olefiodorowa@yandex.ru>
2023-11-05 19:51:34 +0300

    lab3

--- golang/main.go
+++ golang/main.go
@@ -1,22 +1,24 @@
 package main
 
-import ("fmt"
-"math")
+import (
+  "fmt"
+  "math"
+)
 
-func formula (x float64) float64 {
-	a:=2.0
-	return ((math.Pow((math.Log10(a+x),2))/(math.Pow((a+x),2))))
+func formula(x float64) float64 {
+  a :=2.0 
+  return ((math.Pow((math.Log10(a+x)),2)) / (math.Pow((a+x),2)))
 }
 
 func main() {
-	fmt.Println("Fedorova Olga Andreevna")
-	fmt.Println("Задача А")
-	for i:=1.2; i<=4.2; i+=0.6{
-		fmt.Println("При x=", i, "y=", formula(i))
-	}
-	fmt.Println("Задача B")
-	var list[5]float64=[5]float64{1.16, 1.32, 1.47, 1.65, 1.93}
-	for i:=1; i<=4; i++{
-		fmt.Println("При x=", list[i], "y=", formula(list[i]))
-	}
+  fmt.Println("Фёдорова Ольга Андреевна")
+  fmt.Println("Задача А")
+  for i := 1.2; i <= 4.2; i += 0.6 {
+    fmt.Println("x=", i, "y=", formula(i))
+  }
+  fmt.Println("Задача B")
+  var list [5]float64 = [5]float64{1.16, 1.32, 1.47, 1.65, 1.93}
+  for i := 0; i <= 4; i++ {
+    fmt.Println("x=", list[i], "y=", formula(list[i]))
+  }
 }
