13fe44bc375746304a3b5fc5e93eb42231fe536d
Scorp-From-Ivan0v0 <mazurov-danilka@mail.ru>
2023-11-05 18:28:11 +0300

    lab2 + lab3 (correction)

--- golang/main.go
+++ golang/main.go
@@ -25,14 +25,12 @@ func Primer() {
 		x += deltaX
 	}
 	values := []float64{x1, x2, x3, x4, x5}
-
 	for _, x := range values {
 		result := math.Log(math.Abs(b*b-x*x)) / math.Log(e)
 		result = result / math.Pow(math.Abs(x*x-a*a), 1/5.0)
 		formattedResult := fmt.Sprintf("%.3f", result)
 		fmt.Println("При x=", x, " y=", formattedResult)
 	}
-
 }
 func main() {
 	fmt.Println("Мазуров Даниил Алексеевич")
