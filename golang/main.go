ddc34cb7152168950efa2196f77889867745f27b
MarbleTheFox <dviser2005@gmail.com>
2023-11-05 20:47:12 +0300

    lab2 and lab3 (correct)

--- golang/main.go
+++ golang/main.go
@@ -13,11 +13,9 @@ func assignment_1() {
 		x := i
 		var y float64 = (math.Pow((x-a), (2.00/3.00)) + math.Pow(math.Abs(x+b), (1.00/5.00))) / math.Pow((math.Pow(x, 2)-math.Pow((a+b), 2)), (1.00/9.00))
 		res_b = append(res_b, y)
-
 	}
 	fmt.Println(res_b)
 }
-
 func assignment_2() {
 	var a float64 = 0.8
 	var b float64 = 0.4
@@ -30,7 +28,6 @@ func assignment_2() {
 	}
 	fmt.Println(res_b)
 }
-
 func main() {
 	assignment_1()
 	assignment_2()
