1b24a129045d21c0d635dc2a064e323035c8cb4d
Alexey_Ivanov <alexcar1801165@gmail.com>
2023-11-05 22:00:19 +0300

    Lab1_1

---
+++ golang/Lab1_1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func up(x float64) float64 {
+	return math.Log(x*x*x - 1)
+}
+
+func down(x, a, b float64) float64 {
+	arg := a*x*x - b
+	base := 5.0
+	return math.Log(arg) / math.Log(base)
+}
+
+func main() {
+	for x := 1.2; x <= 2.2; x += 0.2 {
+		a := 1.1
+		b := 0.09
+
+		upf := up(x)
+		downf := down(x, a, b)
+
+		result := upf / downf
+
+		fmt.Println("X =", result)
+	}
+}
