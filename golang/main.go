a82b1ef3d683865f2291ef856356a08b976dc053
MaksimPoLozkov <mmaks3375@gmail.com>
2023-11-06 21:32:41 +0300

    Максим Полозков

--- golang/main.go
+++ golang/main.go
@@ -10,16 +10,16 @@ func main() {
 	a := 2.0
 	x_n := 1.2
 	x_k := 4.2
-	var x_z ; 0.6
-	for x := x_n; x <= x_k; x = x_z {
+	x_z := 0.6
+	for x := x_n; x <= x_k; x = x + x_z {
 		var ya = (math.Pow(math.Log10(a+x), 2) / math.Pow(a+x, 2))
 		fmt.Println("Результат задачи A =", ya)
-		break
+
 	}
 	var argument = [5]float64{1.16, 1.32, 1.47, 1.65, 1.93}
 	for z := 0; z < 5; z++ {
 		var yb = (math.Pow(math.Log10(a+argument[z]), 2) / math.Pow(a+argument[z], 2))
 		fmt.Println("Результат задачи B =", yb)
-		break
+
 	}
 }
