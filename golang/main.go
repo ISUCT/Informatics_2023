df00544902af1e44b2de740d92f28db2b3520823
sweetfyx <gyrykirill@gmail.com>
2023-11-11 13:57:15 +0300

    fix lab3 again

--- golang/main.go
+++ golang/main.go
@@ -5,25 +5,21 @@ import (
 	"math"
 )
 
-func task1(x1 float64, x2 float64, deltax float64) {
-	var a1 float64 = 7.2
-	var b1 float64 = 4.2
+func task1(a float64, b float64, x1 float64, x2 float64, deltax float64) {
 	var answersA = []float64{}
 	for x := x1; x <= x2; x += deltax {
-		var y float64 = math.Sqrt((math.Abs(a1 - b1*x)) / (math.Pow(math.Log10(x), 3)))
+		var y float64 = math.Sqrt((math.Abs(a - b*x)) / (math.Pow(math.Log10(x), 3)))
 		answersA = append(answersA, y)
 	}
 	fmt.Println(answersA)
 }
 
-func task2(x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) {
-	var a2 float64 = 7.2
-	var b2 float64 = 4.2
+func task2(a float64, b float64, x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) {
 	var znachX = []float64{x1, x2, x3, x4, x5}
 	var answersB = []float64{}
 	for i := 0; i < len(znachX); i++ {
 		x := znachX[i]
-		var y float64 = math.Sqrt((math.Abs(a2 - b2*x)) / (math.Pow(math.Log10(x), 3)))
+		var y float64 = math.Sqrt((math.Abs(a - b*x)) / (math.Pow(math.Log10(x), 3)))
 		answersB = append(answersB, y)
 	}
 	fmt.Println(answersB)
@@ -31,6 +27,6 @@ func task2(x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) {
 
 func main() {
 	fmt.Println("Kochetkov Kirill")
-	task1(1.81, 5.31, 0.7)
-	task2(2.4, 2.8, 3.9, 4.7, 3.16)
+	task1(7.2, 4.2, 1.81, 5.31, 0.7)
+	task2(7.2, 4.2, 2.4, 2.8, 3.9, 4.7, 3.16)
 }
