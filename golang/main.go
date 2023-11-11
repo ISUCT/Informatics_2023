071a850b692f895f3359295c7002fcdc82e31f1b
Kolpikov_Valerij_Mihajlovich <valery277vvv@mail.ru>
2023-11-11 10:52:03 +0300

    lab1

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main() {
-	fmt.Println("Hello world")
+func main(){
+	a:= 2.0
+	b:= 3.0
+	n:= 0.11
+	k:= 0.36
+	d:= 0.05 
+
+
+for x := n; x <= k; x += d {
+	y:= math.Asin(math.Pow(x, a)) + math.Acos(math.Pow(x, b))
+	fmt.Printf("x = %.2f, y = %.2f\n", x, y)
+}
 }
