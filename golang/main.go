3c79f9123ae767df443b48edc63369f3af5a7dad
RenatZhappuev <RenatZhappuev@yandex.ru>
2023-11-11 12:51:06 +0300

    lab_3(a, b, deltax fixed)

--- golang/main.go
+++ golang/main.go
@@ -7,21 +7,21 @@ import(
 
 
 
-func task1(x1 float64, x2 float64) {
-	var a1 float64 = 1.2
-	var b1 float64 = 0.48
-	var deltax float64 = 0.3
+func task1(a float64, b float64, x1 float64, x2 float64, deltax float64) {
+	a1 := a
+	b1 := b
+	dx := deltax
 	var answersA = []float64{}
-	for x:=x1; x < x2; x += deltax{
+	for x:=x1; x <= x2; x += dx{
 		var y float64 = (math.Pow(b1, 3) + math.Pow(math.Sin(a1*x), 2)) / (math.Acos(x*b1*x) + math.Exp(-x/2))
 		answersA = append(answersA, y)
 	}
 	fmt.Println(answersA)
 }
 
-func task2(x1 float64,x2 float64,x3 float64,x4 float64,x5 float64) {
-	var a2 float64 = 1.2
-	var b2 float64 = 0.48
+func task2(a float64, b float64, x1 float64, x2 float64,x3 float64,x4 float64,x5 float64) {
+	a2 := a
+	b2 := b
 	var znachX = []float64{x1, x2, x3, x4, x5}
 	var answersB = []float64{}
 	for i:=0; i < len(znachX); i++{
@@ -33,6 +33,6 @@ func task2(x1 float64,x2 float64,x3 float64,x4 float64,x5 float64) {
 }
 
 func main() {
-	task1(0.7, 2.2)
-	task2(0.25, 0.36, 0.56, 0.94, 1.28)
+	task1(1.2, 0.48, 0.7, 2.2, 0.3)
+	task2(1.2, 0.48, 0.25, 0.36, 0.56, 0.94, 1.28)
 }
