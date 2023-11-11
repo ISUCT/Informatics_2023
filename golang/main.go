95d3008b7c41178db02a8f420c2b31a019c06d27
UMBRELLA909 <mix.zhuckow2018@yandex.ru>
2023-11-11 16:19:43 +0300

    Lab3FinalRecorrection

--- golang/main.go
+++ golang/main.go
@@ -5,20 +5,11 @@ import (
 	"math"
 )
 
-// fortask1
-var a float64 = 1.1
-var b float64 = 0.09
-var deltaX float64 = 0.2
-
-// fortask2
-var x1 float64 = 1.21
-var x2 float64 = 1.76
-var x3 float64 = 2.53
-var x4 float64 = 3.48
-var x5 float64 = 4.52
-
 func task1(xn float64, xk float64) {
 	for {
+		var a float64 = 1.1
+		var b float64 = 0.09
+		var deltaX float64 = 0.2
 		var x float64 = xn
 		var y float64 = (math.Log10(math.Pow(x, 3)-1) / (math.Log2(a*math.Pow(x, 2)-b) / math.Log2(5)))
 		fmt.Println("ResultsOfTask1:" + fmt.Sprint(y))
@@ -29,10 +20,12 @@ func task1(xn float64, xk float64) {
 	}
 }
 
-func task2() {
+func task2(x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) {
 	peremennie := [5]float64{x1, x2, x3, x4, x5}
 	fmt.Println(peremennie)
 	for _, xp := range peremennie {
+		var a float64 = 1.1
+		var b float64 = 0.09
 		x := (xp)
 		var y float64 = (math.Log10(math.Pow(x, 3)-1) / (math.Log2(a*math.Pow(x, 2)-b) / math.Log2(5)))
 		fmt.Println("ResultsOfTask2:" + fmt.Sprint(y))
@@ -43,5 +36,5 @@ func main() {
 	fmt.Println("Жуков Михаил Андреевич")
 	fmt.Println("17 лет")
 	task1(1.2, 2.2)
-	task2()
+	task2(1.21, 1.76, 2.53, 3.48, 4.52)
 }
