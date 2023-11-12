437433e06d412060ef8e737c04e1d272cd9006bc
Иван Аксёнов <sk4inee@gmail.com>
2023-11-12 21:16:41 +0300

    Added predetermination of slice capacity

--- golang/internal/lab4/lab4.go
+++ golang/internal/lab4/lab4.go
@@ -1,6 +1,8 @@
 package lab4
 
-import "math"
+import (
+	"math"
+)
 
 func ToRadians(deg float64) float64 {
 	return (deg) * (math.Pi / 180.0)
@@ -11,7 +13,7 @@ func Calculate(x float64, b float64) float64 {
 }
 
 func TaskA(begX, finX, detX, b float64) ([]float64, []float64) {
-	var yList = make([]float64, 0)
+	var yList = make([]float64, 0, int(math.Ceil((finX-begX)/detX+1)))
 	var corrXList = make([]float64, 0)
 	for i := begX; i <= finX; i += detX {
 		yList = append(yList, Calculate(i, b))
