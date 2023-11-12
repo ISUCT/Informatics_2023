de611597407233bbf1a07d1059cfa29c1e2f2b17
busiginandrew <busigin37@mail.ru>
2023-11-12 13:17:53 +0300

    fixlab3

--- golang/main.go
+++ golang/main.go
@@ -5,8 +5,7 @@ import (
 	"math"
 )
 
-func taskA(xn float64, xk float64) {
-	var deltax float64 = 0.4
+func taskA(xn float64, xk float64, deltax float64) {
 	var answersy = []float64{}
 	for x := xn; x <= xk; x += deltax {
 		var y float64 = math.Pow(math.Abs(math.Pow(x, 2)-2.5), 1/4) + math.Pow(math.Log10(math.Pow(x, 2)), 1/3)
@@ -27,6 +26,6 @@ func taskB(x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) {
 
 func main() {
 	fmt.Println("Бусыгин Андрей Михайлович")
-	taskA(1.25, 3.25)
+	taskA(1.25, 3.25, 0.4)
 	taskB(1.84, 2.71, 3.81, 4.56, 5.62)
 }
