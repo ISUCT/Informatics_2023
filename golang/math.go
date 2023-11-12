9cb3b6b79dcb0e9b7466df303ca9d68689e1f07e
rusafob <147656000+rusafob@users.noreply.github.com>
2023-11-12 22:08:32 +0300

    math.go

--- golang/math.go
+++ golang/math.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-"fmt"
-"math"
+	"fmt"
+	"math"
 )
-
 func main() {
-startX := 0.11
-endX := 0.36
-step := 0.05
+	startX := 0.11
+	endX := 0.36
+	step := 0.05
 
-x := startX
-for x <= endX {
-result := calculateFunction(x)
-fmt.Printf("x = %.2f, y = %.4f\n", x, result)
-x += step
-}
+	for x := startX; x <= endX; x += step {
+		result := calculateFunction(x)
+		fmt.Printf("x = %.2f, y = %.4f\n", x, result)
+	}
 }
 
 func calculateFunction(x float64) float64 {
-sinCubed := math.Pow(math.Sin(x), 3)
-cosCubed := math.Pow(math.Cos(x), 3)
-lnX := math.Log(x)
+	sinCubed := math.Pow(math.Sin(x), 3)
+	cosCubed := math.Pow(math.Cos(x), 3)
+	lnX := math.Log(x)
 
-return (sinCubed + cosCubed) * lnX
+	return (sinCubed + cosCubed) * lnX
 }
+
+
+
+
