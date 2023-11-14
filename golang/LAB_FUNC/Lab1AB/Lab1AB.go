53306ea3e78fa0a8b0e3495bac070e9a19d985c3
Alexey_Ivanov <alexcar1801165@gmail.com>
2023-11-14 20:55:33 +0300

    LabAB_fixed#4

--- golang/LAB_FUNC/Lab1AB/Lab1AB.go
+++ golang/LAB_FUNC/Lab1AB/Lab1AB.go
@@ -16,11 +16,20 @@ func down(x, a, b float64) float64 {
 }
 
 func main() {
+	fmt.Println("Задача A:")
+	a := 1.1
+	b := 0.09
 	for x := 1.2; x <= 2.2; x += 0.2 {
-		a := 1.1
-		b := 0.09
 		upf := up(x)
 		downf := down(x, a, b)
+		result := upf / downf
+		fmt.Println("X =", result)
+	}
+	fmt.Println("\n Задача B:")
+	var numbers [5]float64 = [5]float64{4.48, 3.56, 2.78, 5.28, 3.21}
+	for x := 0; x <= 4; x++ {
+		upf := up(numbers[x])
+		downf := down(numbers[x], a, b)
 		result := upf / downf
 		fmt.Println("X =", result)
 	}
