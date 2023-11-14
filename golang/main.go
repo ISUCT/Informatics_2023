5a87c2ce9f6dcde0c123a70107735335ce90c535
pvzprrrr <alenanaumovvva@mail.ru>
2023-11-14 22:46:14 +0300

    laba 3 var 14

--- golang/main.go
+++ golang/main.go
@@ -5,6 +5,10 @@ import (
 	"math"
 )
 
+func zadacha(x float64, a float64, b float64) float64 {
+	return (math.Sqrt((math.Abs(a - b*x)) / (math.Pow(math.Log10(x), 3))))
+}
+
 func main() {
 	//Лабораторная 2
 	fmt.Println("Naumova Alena Alekseevna")
@@ -14,21 +18,20 @@ func main() {
 	//Решение под А
 	a := 7.2
 	b := 4.2
-	x__begin := 1.81
-	x__end := 5.31
-	x__delta := 0.7
+	xBegin := 1.81
+	xEnd := 5.31
+	xDelta := 0.7
 	fmt.Println("Решения под A:")
-	for x := x__begin; x <= x__end; x = x + x__delta {
-		var y1 = math.Sqrt((math.Abs(a - b*x)) / (math.Pow(math.Log10(x), 3)))
-		fmt.Println(y1)
+	for y := xBegin; y <= xEnd; y = y + xDelta {
+		fmt.Println(zadacha(y, a, b))
 	}
 
 	//Решение под Б
 	fmt.Println("Решения под B:")
 	var arguments = [5]float64{2.4, 2.8, 3.9, 4.7, 3.16}
-	for n := 0; n < 5; n++ {
-		var y2 = math.Sqrt((math.Abs(a - b*arguments[n])) / (math.Pow(math.Log10(arguments[n]), 3)))
-		fmt.Println(y2)
+	for n := 0; n < len(arguments); n++ {
+		c := arguments[n]
+		fmt.Println(zadacha(c, a, b))
 	}
 
 }
