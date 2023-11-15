99e7766a9ac67374582b1a362bc22e76b2114f9b
IlyaParamonov280 <145789145+IlyaParamonov280@users.noreply.github.com>
2023-11-15 15:21:50 +0300

    Lab3 functions

--- golang/main.go
+++ golang/main.go
@@ -7,10 +7,14 @@ import (
 
 const a, b = 2.0, 1.1
 
-func funkt16(c float64) float64 {
-	result := (math.Log(math.Abs(math.Pow(b, 2) - math.Pow(c, 2)))) / (math.Pow(math.Abs(math.Pow(c, 2)-math.Pow(a, 2)), 0.2))
-	return result
+func funkt16(c float64) float64 { 
+	return (math.Log(math.Abs(math.Pow(b, 2) - math.Pow(c, 2)))) / (math.Pow(math.Abs(math.Pow(c, 2)-math.Pow(a, 2)), 0.2))
 }
+
+func taskB(d float64) float64 {
+	return funkt16(d)
+}
+
 func main() {
 	fmt.Println("Парамонов Илья Сергеевич")
 	fmt.Println("16 вариант")
@@ -19,14 +23,9 @@ func main() {
 		fmt.Println(funkt16(x))
 	}
 	fmt.Println("Задача В")
-	x1 := 0.1
-	fmt.Println(funkt16(x1))
-	x2 := 0.3
-	fmt.Println(funkt16(x2))
-	x3 := 0.4
-	fmt.Println(funkt16(x3))
-	x4 := 0.45
-	fmt.Println(funkt16(x4))
-	x5 := 0.65
-	fmt.Println(funkt16(x5))
+	fmt.Println(taskB(0.1))
+	fmt.Println(taskB(0.3))
+	fmt.Println(taskB(0.4))
+	fmt.Println(taskB(0.45))
+	fmt.Println(taskB(0.65))
 }
