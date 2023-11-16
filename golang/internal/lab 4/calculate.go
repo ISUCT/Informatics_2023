76da7dfdadf4fb01dcdcfae0c8043fb238263337
LysovDmitri <lysovdmitriy37@gmsil.com>
2023-11-16 15:44:56 +0300

    Добавление пакета функций

---
+++ golang/internal/lab 4/calculate.go
@@ -0,0 +1,27 @@
+package lab4
+
+import "math"
+
+func Formula(consta, x float64) float64 {
+	return (math.Pow(consta, (math.Pow(x, 2)-1)) - math.Log10(math.Pow(x, 2)-1) + math.Cbrt((math.Pow(x, 2) - 1)))
+}
+
+func TaskA(consta, start, end, step float64) ([]float64, []float64) {
+	y := []float64{}
+	x := []float64{}
+	for i := start; i <= end; i += step {
+		y = append(y, Formula(consta, i))
+		x = append(x, i)
+	}
+	return x, y
+}
+
+func TuskB(consta float64, xArr []float64) ([]float64, []float64) {
+	y := []float64{}
+	x := []float64{}
+	for _, i := range xArr {
+		y = append(y, Formula(consta, i))
+		x = append(x, i)
+	}
+	return x, y
+}
