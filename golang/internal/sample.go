e8c29677a63eb166e3c1393b5a014c73868f3f1d
Sofia <chursina.sofia37@yandex.ru>
2023-11-17 10:09:49 +0300

    lab4

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -1,5 +1,16 @@
 package internal
 
+import (
+	"math"
+)
+
 func Summ(a, b int) int {
 	return a + b
+}
+func log(base, x float64) float64 {
+	return math.Log(x) / math.Log(base)
+}
+
+func Func_res(a, b, x float64) float64 {
+	return (a*math.Cbrt(x) - b*log(5, x)) / math.Pow(math.Log10(x-1), 3)
 }
--- golang/main.go
+++ golang/main.go
@@ -2,33 +2,26 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal"
 )
-
-func Log(base, x float64) float64 {
-	return math.Log(x) / math.Log(base)
-}
-
-func y(a, b, x float64) float64 {
-	return (a*math.Cbrt(x) - b*Log(5, x)) / math.Pow(math.Log10(x-1), 3)
-}
 
 func main() {
 	fmt.Println("Чурсина Софья 1/278")
 
-	a := 4.1
-	b := 2.7
+	var a float64 = 4.1
+	var b float64 = 2.7
 
 	fmt.Println("Задача A")
 	for x := 1.5; x <= 3.5; x += 0.4 {
-		res := y(a, b, x)
+		res := internal.Func_res(a, b, x)
 		fmt.Println(res)
 	}
 
 	fmt.Println("Задача B")
 	array := [5]float64{1.9, 2.15, 2.34, 2.74, 3.16}
 	for _, v := range array {
-		res := y(a, b, v)
+		res := internal.Func_res(a, b, v)
 		fmt.Println(res)
 	}
 }
