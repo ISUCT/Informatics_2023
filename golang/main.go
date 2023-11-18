ff25d20557eeabfb76a2aea9d572ea7a5419502e
LysovDmitri <lysovdmitriy37@gmsil.com>
2023-11-18 19:59:37 +0300

    lab4

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
+	"isuct.ru/informatics2022/internal/lab4"
+)
+
+// вариант 12
 func main() {
-	fmt.Println("Hello world")
+	var a float64 = 1.6
+	fmt.Println("Задача A")
+	x, y := lab4.TaskA(a, 1.2, 3.7, 0.5)
+	for i, _ := range y {
+		fmt.Println("При x=", x[i], "y=", y[i])
+	}
+	fmt.Println("Задача B")
+	var listik []float64 = []float64{1.28, 1.36, 2.47, 3.68, 4.56}
+	x, y = lab4.TaskB(a, listik)
+	for i, _ := range y {
+		fmt.Println("При x=", x[i], "y=", y[i])
+	}
 }
