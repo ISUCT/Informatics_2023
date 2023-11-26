2d4647f50af0361f205982e295dac8891317bca6
iad <7-data@mmail.ru>
2023-11-26 20:44:35 +0300

    правки в функции

--- golang/main.go
+++ golang/main.go
@@ -2,40 +2,25 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal/lab4"
 )
 
 func main() {
 	fmt.Println("Hello world")
-	fmt.Println("Iakovleva Daria Dmitrievna 1/278")
-	fmt.Printf("\n")
-	fmt.Println("24 вариант")
-	task_A()
-	fmt.Printf("\n")
-	task_B()
-}
+	fmt.Printf("Jakovleva Darja Dmitrievna 1/278\n\n")
 
-func task_A() {
+	fmt.Println("24 вариант")
 	a := 7.2
 	b := 1.3
 	xn := 1.56
 	xk := 4.71
 	dx := 0.63
-	fmt.Println("task_A")
-	for x := xn; x <= xk; x += dx {
-		y := math.Pow(((a + b*x) / (math.Pow((math.Log10(x)), 3))), 1.0/5.0)
-		fmt.Printf("%.2f\t", x)
-		fmt.Printf("%.2f\n", y)
-	}
-}
-func task_B() {
-	a := 7.2
-	b := 1.3
 	vhodnie_x := []float64{2.4, 2.8, 3.9, 4.7, 3.16}
-	fmt.Println("task_B")
-	for _, x := range vhodnie_x {
-		y := math.Pow(((a + b*x) / (math.Pow((math.Log10(x)), 3))), 1.0/5.0)
-		fmt.Printf("%.2f\t", x)
-		fmt.Printf("%.2f\n", y)
-	}
+
+	yA := lab4.Task_A(a, b, xn, xk, dx)
+	yB := lab4.Task_B(a, b, vhodnie_x)
+
+	fmt.Println("Task_A:", yA)
+	fmt.Println("Task_B:", yB)
 }
