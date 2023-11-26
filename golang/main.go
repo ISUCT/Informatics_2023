381b243dd7298d4437ddd41836f68a4959e406ce
sweetfyx <gyrykirill@gmail.com>
2023-11-26 12:25:47 +0300

    fixed commentsd for lab3

--- golang/main.go
+++ golang/main.go
@@ -5,28 +5,27 @@ import (
 	"math"
 )
 
-func task1(a float64, b float64, x1 float64, x2 float64, deltax float64) {
-	var answersA = []float64{}
+func task1(a float64, b float64, x1 float64, x2 float64, deltax float64) []float64 {
+	answersA := make([]float64, 0, 10)
 	for x := x1; x <= x2; x += deltax {
 		var y float64 = math.Sqrt((math.Abs(a - b*x)) / (math.Pow(math.Log10(x), 3)))
 		answersA = append(answersA, y)
 	}
-	fmt.Println(answersA)
+	return answersA
 }
-
-func task2(a float64, b float64, x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) {
-	var znachX = []float64{x1, x2, x3, x4, x5}
-	var answersB = []float64{}
-	for i := 0; i < len(znachX); i++ {
-		x := znachX[i]
+func task2(a float64, b float64, xValues []float64) []float64 {
+	answersB := make([]float64, 0, 10)
+	for _, x := range xValues {
 		var y float64 = math.Sqrt((math.Abs(a - b*x)) / (math.Pow(math.Log10(x), 3)))
 		answersB = append(answersB, y)
 	}
-	fmt.Println(answersB)
+	return answersB
 }
 
 func main() {
 	fmt.Println("Kochetkov Kirill")
-	task1(7.2, 4.2, 1.81, 5.31, 0.7)
-	task2(7.2, 4.2, 2.4, 2.8, 3.9, 4.7, 3.16)
+	results1 := task1(7.2, 4.2, 1.81, 5.31, 0.7)
+	fmt.Println(results1)
+	results2 := task2(7.2, 4.2, []float64{2.4, 2.8, 3.9, 4.7, 3.16})
+	fmt.Println(results2)
 }
