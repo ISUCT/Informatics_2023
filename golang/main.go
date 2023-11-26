b9d0c311637af5cf564e210409bb3f9a6fe46d0a
Alexey_Ivanov <alexcar1801165@gmail.com>
2023-11-26 20:57:18 +0300

    lab1_package_fix

--- golang/main.go
+++ golang/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	fmt.Println("Иванов Алексей Алексеевич 1/278")
 	fmt.Println(internal.SolveA(1.1, 0.09))
 	fmt.Println(internal.SolveB([]float64{4.48, 3.56, 2.78, 5.28, 3.21}))
 }
