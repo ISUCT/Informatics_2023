ebf722c58e7faf62f0fdc8b6cf4e4657a9a13f8d
HaQmUser <pk@dpms.ru>
2023-11-27 19:44:52 +0300

    лаб4 + лаб3 доделал

--- golang/main.go
+++ golang/main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal/lab4"
 )
 
-func formula(x float64) float64 {
-	a := 1.2
-	b := 0.48
-	e := 2.71
-	return ((math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos((x * b * x)) + math.Pow(e, (-x/2))))
-}
 func main() {
 	fmt.Println("Корчагин Пётр Сергеевич")
 	fmt.Println("Ответы на задачу A")
-	for i := 0.7; i <= 2.2; i += 0.3 {
-		fmt.Println("x=", i, "y=", formula(i))
+	xN := 0.7
+	xK := 2.2
+	xD := 0.2
+	x, y := lab4.TaskA(xN, xK, xD)
+	for i := range y {
+		fmt.Println("x=", x[i], "y=", y[i])
 	}
 	fmt.Println("Ответы на задачу B")
-	var listx [5]float64 = [5]float64{0.25, 0.36, 0.56, 0.94, 1.28}
-	for i := 0; i < 5; i++ {
-		fmt.Println("x=", listx[i], "y=", formula(listx[i]))
+	xArray := []float64{0.25, 0.36, 0.56, 0.94, 1.28}
+	x, y = lab4.TaskB(xArray)
+	for i := range y {
+		fmt.Println("x=", x[i], "y=", y[i])
 	}
 }
