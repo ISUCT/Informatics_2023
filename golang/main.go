b5da0424a409ca673bb5fa453933dbdcd8241bab
iad <7-data@mmail.ru>
2023-11-28 20:36:50 +0300

    lab func

--- golang/main.go
+++ golang/main.go
@@ -9,18 +9,7 @@ import (
 func main() {
 	fmt.Println("Hello world")
 	fmt.Printf("Jakovleva Darja Dmitrievna 1/278\n\n")
-
 	fmt.Println("24 вариант")
-	a := 7.2
-	b := 1.3
-	xn := 1.56
-	xk := 4.71
-	dx := 0.63
-	vhodnie_x := []float64{2.4, 2.8, 3.9, 4.7, 3.16}
-
-	yA := lab4.Task_A(a, b, xn, xk, dx)
-	yB := lab4.Task_B(a, b, vhodnie_x)
-
-	fmt.Println("Task_A:", yA)
-	fmt.Println("Task_B:", yB)
+	fmt.Println("Task_A:", lab4.Task_A(7.2, 1.3, 1.56, 4.71, 0.63))
+	fmt.Println("Task_B:", lab4.Task_B(7.2, 1.3, []float64{2.4, 2.8, 3.9, 4.7, 3.16}))
 }
