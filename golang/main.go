ece3d95c1db98e8997151e98deaf9584422996c6
Keezo0 <polinakostina118@gmail.com>
2023-11-28 20:10:43 +0300

    lab 3 fixes

--- golang/main.go
+++ golang/main.go
@@ -5,33 +5,32 @@ import (
 	"math"
 )
 
-func task1(x1 float64, x2 float64) {
-	var a1 float64 = 4.1
-	var b1 float64 = 2.7
-	var deltax float64 = 0.7
-	var answersA = []float64{}
+func calcY(a float64, b float64, x float64) float64 {
+	return (a*math.Sqrt(x) - b*(math.Log(x)/math.Log(5))) / (math.Log10(math.Abs(x - 1)))
+}
+
+func task1(a float64, b float64, x1 float64, x2 float64, deltax float64) {
+	var answersA = make([]float64, 0, int(math.Round(x2-x1)/deltax))
 	for x := x1; x <= x2; x += deltax {
-		var y float64 = (a1*math.Sqrt(x) - b1*(math.Log(x)/math.Log(5))) / (math.Log10(math.Abs(x - 1)))
+		y := calcY(a, b, x)
 		answersA = append(answersA, y)
 	}
 	fmt.Println(answersA)
 }
 
-func task2(x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) {
-	var a2 float64 = 4.1
-	var b2 float64 = 2.7
+func task2(a float64, b float64, x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) {
 	var znachX = []float64{x1, x2, x3, x4, x5}
-	var answersB = []float64{}
+	var answersB = make([]float64, 0, len(znachX))
 	for i := 0; i < len(znachX); i++ {
 		x := znachX[i]
-		var y float64 = (a2*math.Sqrt(x) - b2*(math.Log(x)/math.Log(5))) / (math.Log10(math.Abs(x - 1)))
+		y := calcY(a, b, x)
 		answersB = append(answersB, y)
 	}
 	fmt.Println(answersB)
 }
 
 func main() {
-	fmt.Println("Полина Костина")
-	task1(1.2, 5.2)
-	task2(1.9, 2.15, 2.34, 2.73, 3.16)
+	task1(4.1, 2.7, 1.2, 5.2, 0.7)
+	task2(4.1, 2.7, 1.9, 2.15, 2.34, 2.73, 3.16)
+	fmt.Println("Hello world")
 }
