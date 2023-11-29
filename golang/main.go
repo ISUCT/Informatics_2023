b116353b0060462ce138637a2300630c54f3c74d
UMBRELLA909 <mix.zhuckow2018@yandex.ru>
2023-11-29 18:17:15 +0300

    Lab3Recorrection3

--- golang/main.go
+++ golang/main.go
@@ -5,36 +5,33 @@ import (
 	"math"
 )
 
-func task1(xn float64, xk float64) {
-	for {
-		var a float64 = 1.1
-		var b float64 = 0.09
-		var deltaX float64 = 0.2
-		var x float64 = xn
-		var y float64 = (math.Log10(math.Pow(x, 3)-1) / (math.Log2(a*math.Pow(x, 2)-b) / math.Log2(5)))
-		fmt.Println("ResultsOfTask1:" + fmt.Sprint(y))
-		xn = xn + deltaX
-		if xn >= xk {
-			break
-		}
-	}
+func task1(a float64, b float64, x float64) float64 {
+	return (math.Log10(math.Pow(x, 3)-1) / (math.Log2(a*math.Pow(x, 2)-b) / math.Log2(5)))
 }
 
-func task2(x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) {
-	peremennie := [5]float64{x1, x2, x3, x4, x5}
-	fmt.Println(peremennie)
-	for _, xp := range peremennie {
-		var a float64 = 1.1
-		var b float64 = 0.09
-		x := (xp)
-		var y float64 = (math.Log10(math.Pow(x, 3)-1) / (math.Log2(a*math.Pow(x, 2)-b) / math.Log2(5)))
-		fmt.Println("ResultsOfTask2:" + fmt.Sprint(y))
-	}
+func task2(peremennie []float64, a float64, b float64, x float64) float64 {
+	return (math.Log10(math.Pow(x, 3)-1) / (math.Log2(a*math.Pow(x, 2)-b) / math.Log2(5)))
 }
 
 func main() {
 	fmt.Println("Жуков Михаил Андреевич")
 	fmt.Println("17 лет")
-	task1(1.2, 2.2)
-	task2(1.21, 1.76, 2.53, 3.48, 4.52)
+	//dannie
+	var a float64 = 1.1
+	var b float64 = 0.09
+	//task1
+	var deltaX float64 = 0.2
+	var xn float64 = 1.2
+	var xk float64 = 2.2
+	fmt.Println("AnswerOnTask1:")
+	for x := xn; x <= xk; x += deltaX {
+		fmt.Println(task1(a, b, x))
+	}
+	//task2
+	mySliceForTask2 := []float64{1.21, 1.76, 2.53, 3.48, 4.52}
+	fmt.Println("AnswerOnTask2:")
+	for _, xp := range mySliceForTask2 {
+		x := (xp)
+		fmt.Println(task2(mySliceForTask2, a, b, x))
+	}
 }
