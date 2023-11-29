31f127badcba23ec88519441c4d7f3d973aab75a
evabay01 <evalev2004@yandex.ru>
2023-11-30 01:13:04 +0300

    lab 4

--- golang/main.go
+++ golang/main.go
@@ -2,28 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal"
 )
 
 func main() {
 	// Лабораторная 2
 	fmt.Println("Байгулова Ева Валерьевна")
 
-	// Лабораторная 3 - Вариант 3
-	a := 2.0
-	b := 0.95
-	x_begin := 1.25
-	x_end := 2.75
-	x_delta := 0.3
-	fmt.Println("Значение функции №3 при данных задачи А =")
-	for x := x_begin; x <= x_end; x = x + x_delta {
-		var y1 = (1 + math.Pow(math.Log10(x/a), 2)) / (b - (math.Exp(x / a)))
-		fmt.Println(y1)
-	}
-	var argument = [5]float64{2.2, 3.78, 4.51, 6.58, 1.2} // Массив значений x1-5
-	fmt.Println("Значение функции №3 при данных задачи B =")
-	for i := 0; i < 5; i++ {
-		var y2 = (1 + math.Pow(math.Log10(argument[i]/a), 2)) / (b - (math.Exp(argument[i] / a)))
-		fmt.Println(y2)
-	}
+	// Лабораторная 3,4 - Вариант 3
+	fmt.Println("Значение функции №3 при данных задачи А =", internal.TaskA(1.25, 2.75, 0.3))
+	fmt.Println("Значение функции №3 при данных задачи B =", internal.TaskB([]float64{2.2, 3.78, 4.51, 6.58, 1.2}))
 }
