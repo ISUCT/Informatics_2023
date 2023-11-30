d8d7ba47b703c1c7e9c2b8c689866c70add53f4c
UMBRELLA909 <mix.zhuckow2018@yandex.ru>
2023-11-30 14:40:23 +0300

    Tryingtocorrect2

--- golang/main.go
+++ golang/main.go
@@ -9,7 +9,7 @@ func task1(a float64, b float64, x float64) float64 {
 	return (math.Log10(math.Pow(x, 3)-1) / (math.Log2(a*math.Pow(x, 2)-b) / math.Log2(5)))
 }
 
-func task2(mySliceForTask2 []float64, a float64, b float64, x float64) float64 {
+func task2(_ []float64, a float64, b float64, x float64) float64 {
 	return (math.Log10(math.Pow(x, 3)-1) / (math.Log2(a*math.Pow(x, 2)-b) / math.Log2(5)))
 }
 
