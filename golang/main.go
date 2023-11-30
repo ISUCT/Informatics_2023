4c5a9cfa4c45dd6720739b73fe6a324cfdc3138f
Veronika <sadpeppapig@gmail.com>
2023-11-30 12:26:16 +0300

    add func

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "isuct.ru/informatics2022/internal/lab4"
+)
 
 func main() {
-	fmt.Println("Hello world")
+  fmt.Println("Васильева Вероника Дмитриевна")
+  fmt.Println("Задача А")
+  x,y := lab4.TaskA(1.25,3.25,0.4)
+  for i := range y {
+      fmt.Println("x=", x[i], "y=", y[i])
+  }
+  fmt.Println("Задача B")
+  var list []float64 = []float64{1.84, 2.71, 3.81, 4.56, 5.62}
+  x,y := lab4.TaskA(list)
+  for i := range y{
+      fmt.Println("x=", x[i], "y=", y[i])
+  }
 }
