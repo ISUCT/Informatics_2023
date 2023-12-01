01826b1c09a2b3b1d29660ea88c19a838154acf9
jesussss69 <ari290nka@gmail.com>
2023-12-01 19:01:25 +0300

    lab 4

--- golang/main.go
+++ golang/main.go
@@ -2,32 +2,24 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal/lab4"
 )
 
-func ToRadians(deg float64) float64 {
-	return (deg) * (math.Pi / 180.0)
-}
-
-func Calculate(x float64) float64 {
-	var exp float64
-	exp = (math.Pow(math.Sin(ToRadians(x)), 3) + math.Pow(math.Cos(ToRadians(x)), 3)) * math.Log10(x)
-	return exp
+func prnt(ylist, xlist []float64) {
+	for i, v := range ylist {
+		fmt.Printf("x = %f  y = %f\n", xlist[i], v)
+	}
 }
 
 func main() {
 	fmt.Println("Соколова Арина Михайловна")
-	var begX = 0.11
-	var endX = 0.36
-	var detX = 0.05
 
-	for x := begX; x <= endX; x += detX {
-		fmt.Println(Calculate(x))
-	}
+	//лабараторная работа №3 - 4 вариант 21
+	//TASK 1
+	prnt(lab4.TaskA(0.11, 0.36, 0.05))
 
+	//TASK 2
 	var xList = []float64{0.2, 0.3, 0.38, 0.43, 0.57}
 
-	for i := 0; i < len(xList); i++ {
-		fmt.Println(Calculate(xList[i]))
-	}
-}
+	prnt(lab4.TaskB(xList)xList)
