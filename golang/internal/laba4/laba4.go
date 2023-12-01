a8304bcbb072660f9d6d1246b9b55e0d2c0e99c2
busiginandrew <busigin37@mail.ru>
2023-12-01 22:27:30 +0300

    laba3(rebuild),4

---
+++ golang/internal/laba4/laba4.go
@@ -0,0 +1,27 @@
+package laba4
+
+import (
+	"math"
+)
+
+func Calc(x float64) float64 {
+	return math.Pow(math.Abs(math.Pow(x, 2)-2.5), 1/4) + math.Pow(math.Log10(math.Pow(x, 2)), 1/3)
+}
+func TaskA(xn float64, xk float64, deltax float64) []float64 {
+	var answersy []float64 = make([]float64, 0)
+	for x := xn; x <= xk; x += deltax {
+		y := Calc(x)
+		answersy = append(answersy, y)
+	}
+	return answersy
+}
+
+func TaskB(znX []float64) []float64 {
+	var answersy []float64 = make([]float64, 0)
+	for i := 0; i < len(znX); i++ {
+		x := znX[i]
+		y := Calc(x)
+		answersy = append(answersy, y)
+	}
+	return answersy
+}
--- golang/main.go
+++ golang/main.go
@@ -2,30 +2,14 @@ package main
 
 import (
 	"fmt"
-	"math"
-)
 
-func taskA(xn float64, xk float64, deltax float64) {
-	var answersy = []float64{}
-	for x := xn; x <= xk; x += deltax {
-		var y float64 = math.Pow(math.Abs(math.Pow(x, 2)-2.5), 1/4) + math.Pow(math.Log10(math.Pow(x, 2)), 1/3)
-		answersy = append(answersy, y)
-	}
-	fmt.Println(answersy)
-}
-func taskB(x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) {
-	var znX = [5]float64{x1, x2, x3, x4, x5}
-	var answersy = []float64{}
-	for i := 0; i < len(znX); i++ {
-		x := znX[i]
-		var y float64 = math.Pow(math.Abs(math.Pow(x, 2)-2.5), 1/4) + math.Pow(math.Log10(math.Pow(x, 2)), 1/3)
-		answersy = append(answersy, y)
-	}
-	fmt.Println(answersy)
-}
+	"isuct.ru/informatics2022/internal/laba4"
+)
 
 func main() {
 	fmt.Println("Бусыгин Андрей Михайлович")
-	taskA(1.25, 3.25, 0.4)
-	taskB(1.84, 2.71, 3.81, 4.56, 5.62)
+	a1 := laba4.TaskA(1.25, 3.25, 0.4)
+	a2 := laba4.TaskB([]float64{1.84, 2.71, 3.81, 4.56, 5.62})
+	fmt.Println(a1)
+	fmt.Println(a2)
 }
