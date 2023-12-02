7ea081d3c7f44019ce5b1d8af8755f789404d1dc
mariyayakim <mariyayakimanskaya@gmail.com>
2023-12-02 11:37:34 +0300

    added lab1, lab2, lab3, lab4

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/internal/lab4"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Якиманская Мария Алексеевна")
+
+	a1 := lab4.Task1(7.2, 1.3, 1.56, 4.71, 0.63)
+	fmt.Println(a1)
+
+	a2 := lab4.Task2(7.2, 1.3, []float64{2.4, 2.8, 3.9, 4.7, 3.16})
+	fmt.Println(a2)
 }
