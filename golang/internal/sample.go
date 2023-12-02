4e211ea8afece43a5a520eed65e67a48eab2e87d
User of DK310 <dk310@it.isuct.ru>
2023-12-02 11:27:32 +0300

    packages

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -1,5 +1,31 @@
-package internal
-
-func Summ(a, b int) int {
-	return a + b
+package internal 
+ 
+import "math" 
+ 
+func Summ(a, b int) int { 
+ return a + b 
+} 
+ 
+func calculateFunction(x float64) float64 { 
+ arcsin := math.Asin(x) 
+ arccos := math.Acos(x) 
+ return math.Pow( 
+  math.Pow(arcsin, 2.0)+ 
+   math.Pow(arccos, 4.0), 
+  3.0) 
+} 
+ 
+func CalculateFunctionInRanges(xStart, xEnd, xStep float64) []float64 { 
+ results := make([]float64, 0, 6) 
+ for x := xStart; x <= xEnd; x += xStep { 
+  results = append(results, calculateFunction(x)) 
+ } 
+ return results 
+} 
+func CalculateFunctionWithXValues(xes []float64) []float64 { 
+ results := make([]float64, 0, len(xes)) 
+ for _, x := range xes { 
+  results = append(results, calculateFunction(x)) 
+ } 
+ return results 
 }
