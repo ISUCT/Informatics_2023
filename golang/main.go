f9a57786a9c3a8a6a5aef144c731baee8426dd8e
Scorp-From-Ivan0v0 <mazurov-danilka@mail.ru>
2023-12-02 11:13:32 +0300

    lab4correct3

--- golang/main.go
+++ golang/main.go
@@ -7,17 +7,12 @@ import (
 )
 
 func main() {
-
 	lab4.Zad1()
-
 	values := []float64{0.1, 0.3, 0.4, 0.45, 0.65}
-
 	result := lab4.Zad2(values)
-
 	for i, x := range values {
 		formattedResult := fmt.Sprintf("%.3f", result[i])
 		fmt.Println("При x=", x, " y=", formattedResult)
 	}
 	fmt.Println("Мазуров Даниил Алексеевич")
-
 }
