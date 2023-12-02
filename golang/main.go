3ef1a615777579c97fb05b28e1d381f1f301650e
User of DK310 <dk310@it.isuct.ru>
2023-12-02 12:11:55 +0300

    lab 4

--- golang/main.go
+++ golang/main.go
@@ -2,27 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"isuct.ru/informatics2022/internal"
 )
 
 func main() {
 	fmt.Println("Петров Леонид Алексеевич")
-
-	a := 2.5
-	b := 4.6
-	x_n := 1.1
-	x_k := 3.6
-	x_z := 0.5
-	for x := x_n; x <= x_k; x = x_z {
-		var dz_a = (math.Pow((a + b*x), 2.5)) / (1 + (math.Log10(a + b*x)))
-		fmt.Println(dz_a)
-		break
-	}
-
-	var argument = [5]float64{1.2, 1.28, 1.36, 1.46, 2.35}
-	for i := 0; i < 5; i++ {
-		var dz_b = (math.Pow(a+b*argument[i], 2.5)) / (1 + (math.Log10(a + b*argument[i])))
-		fmt.Println(dz_b)
-		break
-	}
-}
+	fmt.Println(internal.CalculatePoint(1.1, 3.6, 0.5))
+	fmt.Println(internal.CalculateFuncWithArguments(2.5, 4.6, []float64 {1.2, 1.28, 1.36, 1.46, 2.35}))
+}	
