7136fce62be30f63ba994323c7421ddd38274faf
RenatZhappuev <RenatZhappuev@yandex.ru>
2023-12-02 10:48:46 +0300

    lab4(fixed #2)

--- golang/internal/lab3/task1&2.go
+++ golang/internal/lab3/task1&2.go
@@ -2,10 +2,14 @@ package lab3
 
 import ("math")
 
+func Calc(a, b, x float64) float64 {
+	return (math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos(x*b*x) + math.Exp(-x/2))
+}
+
 func Task1(a float64, b float64, x1 float64, x2 float64, deltax float64) []float64{
 	var answersA []float64 = make([]float64, int((x2 - x1)/deltax) + 1)
 	for x:=x1; x <= x2; x += deltax{
-		var y float64 = (math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos(x*b*x) + math.Exp(-x/2))
+		y := Calc(a,b,x)
 		answersA = append(answersA, y)
 	}
 	return answersA
@@ -15,7 +19,7 @@ func Task2(a float64, b float64, znachx[] float64) []float64{
 	var answersB []float64 = make([]float64, len(znachx))
 	for _, value := range znachx{
 		x := value
-		var y float64 = (math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos(x*b*x) + math.Exp(-x/2))
+		y := Calc(a,b,x)
 		answersB = append(answersB, y)
 	}
 	return answersB
