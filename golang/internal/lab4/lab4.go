203456c0ab672427a5613c53346ef039336872dc
sweetfyx <gyrykirill@gmail.com>
2023-12-02 09:29:44 +0300

    Revert "fixes and lab4+lab5"
    This reverts commit ecdee8bee195762d73cf2cf92ff7de09ab45a66e.

--- golang/internal/lab4/lab4.go
+++ golang/internal/lab4/lab4.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Task1(a float64, b float64, x1 float64, x2 float64, deltax float64) []float64 {
-	answersA := make([]float64, 0, int(math.Round(x2-x1)/deltax))
+	answersA := make([]float64, 0, 10)
 	for x := x1; x <= x2; x += deltax {
 		var y float64 = math.Sqrt((math.Abs(a - b*x)) / (math.Pow(math.Log10(x), 3)))
 		answersA = append(answersA, y)
@@ -13,7 +13,7 @@ func Task1(a float64, b float64, x1 float64, x2 float64, deltax float64) []float
 	return answersA
 }
 func Task2(a float64, b float64, xValues []float64) []float64 {
-	answersB := make([]float64, 0, len(xValues))
+	answersB := make([]float64, 0, 10)
 	for _, x := range xValues {
 		var y float64 = math.Sqrt((math.Abs(a - b*x)) / (math.Pow(math.Log10(x), 3)))
 		answersB = append(answersB, y)
