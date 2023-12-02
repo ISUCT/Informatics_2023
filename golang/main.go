9b1c10eb5081cc5cef69537c8200a84956d6db60
kotleta96 <golosova_29_04@mail.ru>
2023-12-02 11:03:29 +0300

    lab4

--- golang/main.go
+++ golang/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math"
-)
 
-func result(a float64, b float64, x float64) float64 {
-	return ((math.Cbrt(a*x - b)) / (math.Pow(math.Log10(x), 2)))
-}
+	"isuct.ru/informatics2022/lab4"
+)
 
 func main() {
 	fmt.Println("Голосова Анастасия Сергеевна")
@@ -18,14 +15,5 @@ func main() {
 	x_end := 4.24
 	x_delta := 0.62
 
-	fmt.Println("Решение задачи под буквой А")
-	for x := x_start; x < x_end; x += x_delta {
-		fmt.Println(result(a, b, x))
-	}
-
-	fmt.Println("Решение задачи под буквой B")
-	var values = [5]float64{0.35, 1.28, 3.51, 5.21, 4.16}
-	for i := 0; i < len(values); i++ {
-		fmt.Println(result(a, b, values[i]))
-	}
+	lab4.Solution(a, b, x_start, x_end, x_delta)
 }
