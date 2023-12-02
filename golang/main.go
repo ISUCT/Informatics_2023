8303e7b8588a9b1ed27621402a0f4606535a341b
Kolpikov_Valerij_Mihajlovich <valery277vvv@mail.ru>
2023-12-02 10:57:35 +0300

    lab3

--- golang/main.go
+++ golang/main.go
@@ -1,20 +1,35 @@
 package main
 
+// Задача под А
 import (
 	"fmt"
 	"math"
 )
 
-func main(){
-	a:= 2.0
-	b:= 3.0
-	n:= 0.11
-	k:= 0.36
-	d:= 0.05 
+func main() {
+	a1 := 2.0
+	b1 := 3.0
+	n := 0.11
+	k := 0.36
+	d := 0.05
 
+	for x1 := n; x1 <= k; x1 += d {
+		y1 := math.Asin(math.Pow(x1, a1)) + math.Acos(math.Pow(x1, b1))
+		fmt.Printf("x1 = %.2f, y1 = %.2f\n", x1, y1)
+	}
 
-for x := n; x <= k; x += d {
-	y:= math.Asin(math.Pow(x, a)) + math.Acos(math.Pow(x, b))
-	fmt.Printf("x = %.2f, y = %.2f\n", x, y)
-}
+	// Задача под Б
+
+	a2 := 2.0
+	b2 := 3.0
+	x2 := []float64{0.08, 0.26, 0.35, 0.41, 0.53}
+	y2 := make([]float64, 5)
+
+	for i := 0; i < len(x2); i++ {
+		y2[i] = math.Asin(math.Pow(x2[i], a2)) + math.Acos(math.Pow(x2[i], b2))
+	}
+
+	for i := 0; i < len(x2); i++ {
+		fmt.Printf("x2%d = %.2f, y2%d = %.4f\n", i+1, x2[i], i+1, y2[i])
+	}
 }
