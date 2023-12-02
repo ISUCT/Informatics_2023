ec851971b273bb6cdfcbb7426d8be66107fba157
Latokes <diiscord@mail.ru>
2023-12-02 15:05:37 +0300

    Lab4 Removed whitespaces

--- golang/main.go
+++ golang/main.go
@@ -7,17 +7,13 @@ import (
 )
 
 func main() {
-
 	//Лабораторная 2
 	fmt.Println("Коротков Данила Максимович")
-
 	//Лабораторная 3,4 (9 Вариант)
-
 	a := 1.1
 	b := 0.09
 	x_begin := 1.2
 	x_end := 2.2
 	x_delta := 0.2
-
 	lab4.Solve(a, b, x_begin, x_end, x_delta)
 }
