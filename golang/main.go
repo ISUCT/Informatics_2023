e6ad012f296768e71fce0d6aa00a2fe1f590f72c
sweetfyx <gyrykirill@gmail.com>
2023-12-02 08:42:56 +0300

    removed lab 5

--- golang/main.go
+++ golang/main.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	dishstruct "isuct.ru/informatics2022/internal/dishStruct"
 	"isuct.ru/informatics2022/internal/lab4"
 )
-
-func checkForError(err error) {
-	if err != nil {
-		log.Fatal("Card declined")
-	}
-}
 
 func main() {
 	fmt.Println("Kochetkov Kirill")
@@ -22,15 +14,4 @@ func main() {
 
 	res2 := lab4.Task2(7.2, 4.2, []float64{2.4, 2.8, 3.9, 4.7, 3.16})
 	fmt.Println(res2)
-
-	dish := dishstruct.NewDish(5000, "soup", "Borsch")
-
-	var err error = dish.SetPrice(4999)
-	checkForError(err)
-
-	dish.SetName("Sorrel")
-
-	fmt.Printf("Your dish's price is %d\n", dish.GetPrice())
-	fmt.Printf("Its type is %s\n", dish.GetVid())
-	dish.TipTheWaiter()
 }
