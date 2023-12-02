58845af095f427b8d065445da2c41d66957b62df
UMBRELLA909 <mix.zhuckow2018@yandex.ru>
2023-12-02 15:28:45 +0300

    lab3+4correction

--- golang/main.go
+++ golang/main.go
@@ -3,29 +3,19 @@ package main
 import (
 	"fmt"
 
-	lab4test "isuct.ru/informatics2022/internal"
+	lab4test "isuct.ru/informatics2022/internal/lab4"
 )
 
 func main() {
 	fmt.Println("Жуков Михаил Андреевич")
 	fmt.Println("17 лет")
-	//dannie
-	var a float64 = 1.1
-	var b float64 = 0.09
 	//task1
-	var deltaX float64 = 0.2
-	var xn float64 = 1.2
-	var xk float64 = 2.2
 	fmt.Println("AnswerOnTask1:")
-	for x := xn; x <= xk; x += deltaX {
-		fmt.Println(lab4test.Task1(a, b, x))
-	}
+	resultSliceTaskA := lab4test.Task1Slice(1.1, 0.09, 1.2, 2.2, 0.2)
+	fmt.Println(resultSliceTaskA)
 	//task2
-	mySliceForTask2 := []float64{1.21, 1.76, 2.53, 3.48, 4.52}
+	mySliceForTaskB := []float64{1.21, 1.76, 2.53, 3.48, 4.52}
 	fmt.Println("AnswerOnTask2:")
-	fmt.Println(mySliceForTask2)
-	for _, xp := range mySliceForTask2 {
-		x := (xp)
-		fmt.Println(lab4test.Task2(mySliceForTask2, a, b, x))
-	}
+	resultSlice := lab4test.TaskB(mySliceForTaskB, 1.1, 0.09)
+	fmt.Println(resultSlice)
 }
