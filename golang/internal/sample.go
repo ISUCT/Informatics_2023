5b020bc276dc36e08ec45e5771714fc2bab17412
ritique <zhukov_daniil@bk.ru>
2023-12-02 16:53:30 +0300

    Added lab4

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -1,5 +1,31 @@
 package internal
 
-func Summ(a, b int) int {
-	return a + b
+import (
+	"math"
+)
+
+func radians(deg float64) float64 {
+	return (deg) * (math.Pi / 180)
+}
+
+func calculate(x float64) float64 {
+	return (math.Pow(0.48, 3) + math.Pow(math.Sin(radians(1.2 * x)), 2)) / (math.Acos(x * 0.48 * x) + math.Pow(math.E, -x/2))
+}
+
+func TaskA(x_start, x_end, x_delta float64) []float64 {
+	var cap float64 = (x_end - x_start) / x_delta
+	var cap_int int = int(cap)
+	answerA := make([]float64, 0, cap_int)
+	for i := x_start; i <= x_end; i += x_delta {
+		answerA = append(answerA, calculate(i))
+	}
+	return answerA
+} 
+
+func TaskB(xes []float64) []float64 {
+	answerB := make([]float64, 0, len(xes))
+	for _, i:= range xes {
+		answerB = append(answerB, calculate(i))
+	}
+	return answerB
 }
--- golang/main.go
+++ golang/main.go
@@ -2,30 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"isuct.ru/informatics2022/internal"
 )
 
-func Radians(deg float64) float64 {
-	return (deg) * (math.Pi / 180)
-}
-
-func Calculate(x, a, b float64) float64 {
-	return (math.Pow(b, 3) + math.Pow(math.Sin(Radians(a*x)), 2)) / (math.Acos(x*b*x) + math.Pow(math.E, -x/2))
-}
-
-func main() {
-	a := 1.2
-	b := 0.48
-	x_start := 0.7
-	x_end := 2.2
-	x_delta := 0.3
-	fmt.Println("Задача A")
-	for i := x_start; i <= x_end; i += x_delta {
-		fmt.Println(Calculate(i, a, b))
-	}
-	var Xes = [5]float64{0.25, 0.36, 0.56, 0.94, 1.28}
-	fmt.Println("Задача B")
-	for i := 0; i < len(Xes); i++ {
-		fmt.Println(Calculate(Xes[i], a, b))
-	}
+func main(){
+	fmt.Println(internal.TaskA(0.7, 2.2, 0.3))
+	fmt.Println(internal.TaskB([]float64{0.25, 0.36, 0.56, 0.94, 1.28}))
 }
