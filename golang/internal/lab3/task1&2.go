97c6ba8fd583f2bd0e2221ea1cfee18ac4303715
RenatZhappuev <RenatZhappuev@yandex.ru>
2023-12-02 07:58:57 +0300

    lab4(fixed)

--- golang/internal/lab3/task1&2.go
+++ golang/internal/lab3/task1&2.go
@@ -3,7 +3,7 @@ package lab3
 import ("math")
 
 func Task1(a float64, b float64, x1 float64, x2 float64, deltax float64) []float64{
-	var answersA []float64 = make([]float64, 0)
+	var answersA []float64 = make([]float64, int((x2 - x1)/deltax) + 1)
 	for x:=x1; x <= x2; x += deltax{
 		var y float64 = (math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos(x*b*x) + math.Exp(-x/2))
 		answersA = append(answersA, y)
@@ -11,8 +11,8 @@ func Task1(a float64, b float64, x1 float64, x2 float64, deltax float64) []float
 	return answersA
 }
 
-func Task2(a float64, b float64, znachx[5] float64) []float64{
-	var answersB = []float64{}
+func Task2(a float64, b float64, znachx[] float64) []float64{
+	var answersB []float64 = make([]float64, len(znachx))
 	for _, value := range znachx{
 		x := value
 		var y float64 = (math.Pow(b, 3) + math.Pow(math.Sin(a*x), 2)) / (math.Acos(x*b*x) + math.Exp(-x/2))
--- golang/main.go
+++ golang/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	a1 := lab3.Task1(1.2, 0.48, 0.7, 2.2, 0.3)
-	a2 := lab3.Task2(1.2, 0.48, [5]float64{0.25, 0.36, 0.56, 0.94, 1.28})
+	a2 := lab3.Task2(1.2, 0.48, []float64{0.25, 0.36, 0.56, 0.94, 1.28})
 	fmt.Println(a1)
 	fmt.Println(a2)
 }
