fe2faece70dd38c569f76775c609995f1b964856
AlenaY2312 <152083878+AlenaY2312@users.noreply.github.com>
2023-12-02 10:59:09 +0300

    lab1,2,3,4

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/internal/laba4"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Яковлева Алёна")
+	y1 := laba4.Task1(0.22, 0.92, 0.14)
+	y2 := laba4.Task2([]float64{0.1, 0.35, 0.4, 0.55, 0.6})
+	fmt.Println(y1)
+	fmt.Println(y2)
 }
