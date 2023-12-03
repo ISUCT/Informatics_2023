6079d98ec78926640e195882ac4ca7b3c8e8a9ad
Keezo0 <polinakostina118@gmail.com>
2023-12-03 14:51:37 +0300

    lab4

--- golang/internal/lab4/lab4.go
+++ golang/internal/lab4/lab4.go
@@ -4,21 +4,22 @@ import (
 	"math"
 )
 
+func calcY(a float64, b float64, x float64) float64 {
+	return (a*math.Sqrt(x) - b*(math.Log(x)/math.Log(5))) / (math.Log10(math.Abs(x - 1)))
+}
 func Task1(a float64, b float64, x1 float64, x2 float64, deltax float64) []float64 {
 	var answersA = make([]float64, 0, int(math.Round(x2-x1)/deltax))
 	for x := x1; x <= x2; x += deltax {
-		var y float64 = (a*math.Sqrt(x) - b*(math.Log(x)/math.Log(5))) / (math.Log10(math.Abs(x - 1)))
+		var y float64 = calcY(a, b, x)
 		answersA = append(answersA, y)
 	}
 	return answersA
 }
 
-func Task2(a float64, b float64, x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) []float64 {
-	var znachX = []float64{x1, x2, x3, x4, x5}
-	var answersB = make([]float64, 0, len(znachX))
-	for i := 0; i < len(znachX); i++ {
-		x := znachX[i]
-		var y float64 = (a*math.Sqrt(x) - b*(math.Log(x)/math.Log(5))) / (math.Log10(math.Abs(x - 1)))
+func Task2(a float64, b float64, xValues []float64) []float64 {
+	var answersB = make([]float64, 0, len(xValues))
+	for _, x := range xValues {
+		var y float64 = calcY(a, b, x)
 		answersB = append(answersB, y)
 	}
 	return answersB
--- golang/main.go
+++ golang/main.go
@@ -3,23 +3,13 @@ package main
 import (
 	"fmt"
 
-	"isuct.ru/informatics2022/internal/employeestruct"
 	"isuct.ru/informatics2022/internal/lab4"
 )
 
 func main() {
 	n1 := lab4.Task1(4.1, 2.7, 1.2, 5.2, 0.7)
 	fmt.Println(n1)
-	n2 := lab4.Task2(4.1, 2.7, 1.9, 2.15, 2.34, 2.73, 3.16)
+	n2 := lab4.Task2(4.1, 2.7, []float64{1.9, 2.15, 2.34, 2.73, 3.16})
 	fmt.Println(n2)
-
-	employee := employeestruct.Newemployee("Иван Иванов", 30, "01/01/2023")
-
-	employee.SetName("Павел Павлов")
-
-	fmt.Printf("Employee's age is %d\n", employee.Getage())
-	fmt.Printf("Eployee's name is %s\n", employee.Getname())
-	fmt.Printf("Date is %s\n", employee.Getdate())
-
 	fmt.Println("Полина Костина")
 }
