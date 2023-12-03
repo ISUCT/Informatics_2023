d7dd177bd7f12d639274ee4f78ae945476043226
DasaSemenova <Iva-1403@yandex.ru>
2023-12-03 17:24:59 +0300

    Laba

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -4,7 +4,7 @@ import "math"
 
 func Summ(a, b int) int {
 	return a + b
-}
+
 	a := 2.5
 	b := 4.6
 	x_n := 1.1
@@ -15,14 +15,13 @@ func Summ(a, b int) int {
 	x3 := 1.36
 	x4 := 1.46
 	x5 := 2.35
-{
+
 	for x := x_n; x <= x_k; x = x + x_z 
 		var y1 = (math.Pow((a + b*x), 2.5)) / (1 + (math.Log10(a + b*x)))
-}
-{
+
+
 		var argument = [5]float64{x1, x2, x3, x4, x5} // Массив значений x1-5
-}
-{	
+
+	
 		for i := 0; i < 5; i++ 
 			var y2 = (math.Pow((a + b*x), 2.5)) / (1 + (math.Log10(a + b*x)))
-}
