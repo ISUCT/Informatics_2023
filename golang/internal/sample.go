35bbd1e30adf8ccd3eeb9b6df24ab4782fc55b74
Alexey_Ivanov <alexcar1801165@gmail.com>
2023-12-04 00:24:48 +0300

    added_make

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -24,7 +24,7 @@ func up(x float64) float64 {
 
 func SolveB(numbers []float64) []float64 {
 	fmt.Println("\n Задача B:")
-	var resultB []float64
+	var resultB []float64 = make([]float64, 0, 5)
 	for x := 0; x <= 4; x++ {
 		upf := up(numbers[x])
 		downf := down(numbers[x], a, b)
@@ -35,7 +35,7 @@ func SolveB(numbers []float64) []float64 {
 
 func SolveA(a, b float64) []float64 {
 	fmt.Println("Задача A:")
-	var resultA []float64
+	var resultA []float64 = make([]float64, 0, 6)
 	for x := 1.2; x <= 2.2; x += 0.2 {
 		upf := up(x)
 		downf := down(x, a, b)
