97cb53cda8755e417eb5ee96363eaa66d387a926
tovarishchgamma <chudov-2006@mail.ru>
2023-12-03 14:19:57 +0300

    Lab4.correction

--- golang/main.go
+++ golang/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	r_a := Lab4.Part_1()
-	r_b := Lab4.Part_2()
+	r_a := Lab4.Part_1(2.0, 4.1)
+	r_b := Lab4.Part_2(2.0, 4.1)
 	fmt.Println(r_a)
 	fmt.Println(r_b)
 	fmt.Println("Чудов Даниил")
