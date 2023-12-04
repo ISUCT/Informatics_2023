ffc544e0934debbf7a6fe2b65375cd3f582bd2a1
ivoxytryer <nik.aminov.2000@mail.ru>
2023-12-04 19:31:31 +0300

    labaaa

--- golang/main.go
+++ golang/main.go
@@ -2,27 +2,83 @@ package main
 
 import (
 	"fmt"
-
-	"isuct.ru/informatics2022/internal/lab4"
+	"log"
+	"os"
 )
 
-func prnt(xL []float64, yL []float64) {
-	for i := range yL {
-		fmt.Printf("x = %.2f, y = %f\n", xL[i], yL[i])
+func errorCheck(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type fox struct {
+	age      int
+	species  string
+	isHungry bool
+}
+
+func newFox(age int, species string, isHungry bool) (fox, error) {
+	var f fox = fox{
+		isHungry: isHungry,
+		species:  species,
+	}
+	var err = f.setAge(age)
+	return f, err
+}
+
+func (f *fox) setAge(age int) error {
+	if age >= 0 && age <= 15 {
+		f.age = age
+		return nil
+	}
+	return fmt.Errorf("fox has invalid age")
+}
+
+func (f fox) getAge() int {
+	return f.age
+}
+
+func (f fox) getSpeices() string {
+	return f.species
+}
+
+func (f fox) getHunger() bool {
+	return f.isHungry
+}
+
+func (f fox) getView() error {
+	asciiCode, err := os.ReadFile("Fox ASCII.txt")
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(asciiCode))
+	return nil
+}
+
+func (f *fox) feedTheFox() {
+	if f.isHungry {
+		f.isHungry = false
+		fmt.Printf("%s is not hungry anymore", f.species)
+	} else {
+		fmt.Printf("%s doesn't want to eat", f.species)
 	}
 }
 
 func main() {
-	fmt.Println("Aminov_Nikita_Andreevich")
+	redFox, err := newFox(2, "Red Fox", false)
+	errorCheck(err)
+	fennecFox, err := newFox(3, "Fennec Fox", true)
+	errorCheck(err)
 
-	//lab3 + 4 - вариант 1
-	//task 1
-	var a = 1.35
-	var b = 0.98
-	prnt(lab4.TaskA(1.14, 4.24, 0.62, a, b))
+	redFox.getView()
+	fmt.Printf("This fox is %d years old\n", redFox.getAge())
+	fmt.Printf("This fox's species is %s\n", redFox.getSpeices())
 
-	//task 2
-	var xList = []float64{0.35, 1.28, 3.51, 5.21, 4.16}
+	fmt.Println("---")
 
-	prnt(xList, lab4.TaskB(xList, a, b))
+	fmt.Printf("This fox's species is %s\n", fennecFox.getSpeices())
+	fennecFox.feedTheFox()
 }
