04b997b3a1e589ca1853dac21883ca8678a63852
MarkVRKS <143934528+MarkVRKS@users.noreply.github.com>
2023-12-04 14:09:32 +0300

    Update sample.go
    Убрал лишнее, теперь вывод результата проходит через импортирование этой папки

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -28,22 +28,3 @@ func Srez(nums []float64) []float64{
 	return nums_of_srez
 }
 
-func fnk2() {
-	fmt.Println("1  Вариант")
-	fmt.Println("Задача А")
-	for x := 1.28; x <= 3.28; x += 0.4 {
-		fmt.Println(fnk1(x))
-	}
-	fmt.Println("Задача B")
-	x1 := 1.1
-	fmt.Println(fnk1(x1))
-	x2 := 2.4
-	fmt.Println(fnk1(x2))
-	x3 := 3.6
-	fmt.Println(fnk1(x3))
-	x4 := 1.7
-	fmt.Println(fnk1(x4))
-	x5 := 3.9
-	fmt.Println(fnk1(x5))
-
-}
