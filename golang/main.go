2248d3d4fc75ced5928cc42eebae65cb325fdf71
Vladislav3707 <mr8815542@gmail.com>
2023-12-04 13:52:07 +0300

    lab 3 correction - lint + lab 4

--- golang/main.go
+++ golang/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
 	lab4 "isuct.ru/informatics2022/internal/lab4"
-	"isuct.ru/informatics2022/internal/lab4/lab5"
 )
 
 func main() {
@@ -16,14 +13,4 @@ func main() {
 	fmt.Println(results)
 	lab4.Primer2(myslicefortask2)
 	fmt.Println(myslicefortask2)
-
-	mouse, err := lab5.NewMouse(0.02, 1, 10.5)
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Вес мышки:", mouse.GetWeight())
-	fmt.Println("Возраст мышки:", mouse.GetAge())
-	fmt.Println("Длина мышки:", mouse.GetLength())
 }
