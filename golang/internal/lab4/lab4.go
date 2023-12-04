5b3b2bd45a82bee04bf7996860d35766a6f15984
Akim Kulbakin <kulbakinakim@gmail.com>
2023-12-05 00:35:56 +0300

    lab 4

--- golang/internal/lab4/lab4.go
+++ golang/internal/lab4/lab4.go
@@ -9,26 +9,25 @@ func Log5(x float64) float64 {
 	return (math.Log(x) / math.Log(base))
 }
 
-func form(x float64) float64 {
-	a := 1.1
-	b := 0.09
+func form(x, a, b float64) float64 {
 	return math.Log10(x*x-1) / Log5(a*math.Pow(x, 2)-b)
 }
-func TaksA(xn, xk, dx float64) ([]float64, []float64) {
-	y := []float64{}
-	x := []float64{}
+func TaksA(xn, xk, dx, a, b float64) ([]float64, []float64) {
+	y := make([]float64, 0, (int((xk-xn)/dx - 1)))
+	x := make([]float64, 0, (int((xk-xn)/dx - 1)))
 	for i := xn; i <= xk; i += dx {
 		x = append(x, i)
-		y = append(y, form(i))
+		y = append(y, form(i, a, b))
 	}
 	return x, y
 }
-func TaskB(xarray []float64) ([]float64, []float64) {
-	y := []float64{}
-	x := []float64{}
+func TaskB(xarray []float64, a, b float64) ([]float64, []float64) {
+
+	x := make([]float64, 0, len(xarray))
+	y := make([]float64, 0, len(xarray))
 	for i := 0; i < len(xarray); i++ {
 		x = append(x, xarray[i])
-		y = append(y, form(xarray[i]))
+		y = append(y, form(xarray[i], a, b))
 	}
 	return x, y
 }
--- golang/main.go
+++ golang/main.go
@@ -12,13 +12,15 @@ func main() {
 	xn := 1.2
 	xk := 2.2
 	dx := 0.2
-	x, y := lab4.TaksA(xn, xk, dx)
+	a := 1.1
+	b := 0.09
+	x, y := lab4.TaksA(xn, xk, dx, a, b)
 	for i := range y {
 		fmt.Println("при x =", x[i], "y =", y[i])
 	}
 	fmt.Println("Задача B")
 	xarray := []float64{1.21, 1.76, 2.53, 3.48, 4.52}
-	x, y = lab4.TaskB(xarray)
+	x, y = lab4.TaskB(xarray, a, b)
 	for i := range y {
 		fmt.Println("при x =", x[i], "y =", y[i])
 	}
