269c87c81b3aae103cb0a912e940fb3dca769eb6
Rodin Vladislav <vladislavr627@gmail.com>
2023-12-04 22:50:41 +0300

    lab4

--- golang/main.go
+++ golang/main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal/lab4"
 )
 
-func formula(x float64) float64 {
-	a := 2.0
-	b := 1.1
-	return (math.Log(math.Abs(math.Pow(b, 2)-math.Pow(x, 2))) / (math.Pow(math.Abs(math.Pow(x, 2)-math.Pow(a, 2)), 0.2)))
-}
 func main() {
 	fmt.Println("Родин Владислав Евгеньевич")
+	a := 2.0
+	b := 1.1
+	Xn := 0.08
+	Xa := 1.08
+	step := 0.2
+	list := []float64{0.1, 0.3, 0.4, 0.45, 0.65}
 	fmt.Println("Задача А")
-	for i := 0.08; i <= 1.08; i += 0.2 {
-		fmt.Println("При x=", i, "y=", formula(i))
+	x, y := lab4.TaskA(Xn, Xa, step, a, b)
+	for i := range y {
+		fmt.Println("При x=", x[i], "y=", y[i])
 	}
 	fmt.Println("Задача B")
-	var list [5]float64 = [5]float64{0.1, 0.3, 0.4, 0.45, 0.65}
-	for i := 0; i <= 4; i++ {
-		fmt.Println("При x=", list[i], "y=", formula(list[i]))
+	x, y = lab4.TaskB(list, a, b)
+	for i := range y {
+		fmt.Println("При x=", x[i], "y=", y[i])
 	}
 }
