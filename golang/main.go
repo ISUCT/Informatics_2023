a53c7cee218c81110c68dfb5285f0383be892db3
IlyaParamonov280 <145789145+IlyaParamonov280@users.noreply.github.com>
2023-12-04 23:56:00 +0300

    Lab4 main

--- golang/main.go
+++ golang/main.go
@@ -2,30 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal"
 )
 
-const a, b = 2.0, 1.1
-
-func funkt16(c float64) float64 { 
-	return (math.Log(math.Abs(math.Pow(b, 2) - math.Pow(c, 2)))) / (math.Pow(math.Abs(math.Pow(c, 2)-math.Pow(a, 2)), 0.2))
-}
-
-func taskB(d float64) float64 {
-	return funkt16(d)
-}
-
 func main() {
-	fmt.Println("Парамонов Илья Сергеевич")
+	fmt.Println("Парамонов Илья Сергеевич 1/280")
 	fmt.Println("16 вариант")
 	fmt.Println("Задача А")
-	for x := 0.08; x <= 1.08; x += 0.2 {
-		fmt.Println(funkt16(x))
-	}
-	fmt.Println("Задача В")
-	fmt.Println(taskB(0.1))
-	fmt.Println(taskB(0.3))
-	fmt.Println(taskB(0.4))
-	fmt.Println(taskB(0.45))
-	fmt.Println(taskB(0.65))
+	fmt.Println(internal.TaskA(0.08, 1.08, 0.2))
+	fmt.Println("Задача B")
+	fmt.Println(internal.TaskB([]float64{0.1, 0.3, 0.4, 0.45, 0.65}))
 }
