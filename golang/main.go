cea9d8c01b8e5a777406457a45844bc763959dc9
Valery277vvv <valery277vvv@mail.ru>
2023-12-05 03:45:38 +0300

    lab4

--- golang/main.go
+++ golang/main.go
@@ -1,35 +1,13 @@
 package main
 
-// Задача под А
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal/lab4"
 )
 
+// 11 вариант
 func main() {
-	a1 := 2.0
-	b1 := 3.0
-	n := 0.11
-	k := 0.36
-	d := 0.05
-
-	for x1 := n; x1 <= k; x1 += d {
-		y1 := math.Asin(math.Pow(x1, a1)) + math.Acos(math.Pow(x1, b1))
-		fmt.Printf("x1 = %.2f, y1 = %.2f\n", x1, y1)
-	}
-
-	// Задача под Б
-
-	a2 := 2.0
-	b2 := 3.0
-	x2 := []float64{0.08, 0.26, 0.35, 0.41, 0.53}
-	y2 := make([]float64, 5)
-
-	for i := 0; i < len(x2); i++ {
-		y2[i] = math.Asin(math.Pow(x2[i], a2)) + math.Acos(math.Pow(x2[i], b2))
-	}
-
-	for i := 0; i < len(x2); i++ {
-		fmt.Printf("x2%d = %.2f, y2%d = %.4f\n", i+1, x2[i], i+1, y2[i])
-	}
+	fmt.Println("Task A", lab4.A(0.11, 0.36, 0.05))
+	fmt.Println("Task B", lab4.B([]float64{0.08, 0.26, 0.35, 0.41, 0.53}))
 }
