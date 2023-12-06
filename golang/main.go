19b130fb7e0efa7615245e6fa2b1a1a2817daae5
J0es1ick <mrjoeslick@gmail.com>
2023-12-07 02:10:10 +0300

    lab5 var12

--- golang/main.go
+++ golang/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"isuct.ru/informatics2022/lab3"
+	"isuct.ru/informatics2022/personStruct"
 )
+
+func ErrorTest(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	fmt.Println("Кремнев Юрий Юрьевич")
@@ -15,4 +23,29 @@ func main() {
 	x_delta := 0.5
 
 	lab3.Solve(a,x_begin,x_end,x_delta)
+
+	person1, err1, err2 := personStruct.NewPerson("John", "M", 20)
+	ErrorTest(err1)
+	ErrorTest(err2)
+
+	person2, err1, err2 := personStruct.NewPerson("Jessica", "Unknown", 19)
+	ErrorTest(err1)
+	ErrorTest(err2)
+
+	person1.SetAge(25)
+	err1 = person1.SetAge(25)
+	ErrorTest(err1)
+
+	person2.SetGender("F")
+	err1 = person2.SetGender("F")
+	ErrorTest(err1)
+
+	fmt.Printf("%s's age - %d.\n", person1.GetName(), person1.GetAge())
+	person2.KnowGender()
+
+	str, err1 := person2.GoToWalk(12)
+	if err1 == nil {
+		fmt.Print(str)
+	}
+	ErrorTest(err1)
 }
---
+++ golang/personStruct/personStruct.go
@@ -0,0 +1,59 @@
+package personStruct
+
+import "fmt"
+
+type person struct {
+	name string
+	age int
+	gender string
+}
+
+func (p person) GetName() string {
+	return p.name
+}
+
+func (p person) GetAge() int {
+	return p.age
+}
+
+func (p person) GetGender() string {
+	return p.gender
+}
+
+func (p *person) SetAge(age int) error {
+	if age > 0 && age < 122 {
+		p.age = age
+		return nil
+	}
+	return fmt.Errorf("age is higher or lower than bound")
+}
+
+func (p *person) SetGender(gender string) error {
+	if gender == "M" || gender == "F" || gender == "Other" || gender == "Unknown" {
+		p.gender = gender
+		return nil
+	}
+	return fmt.Errorf("gender can only be M/F/Other")
+}
+
+func NewPerson(Name, Gender string, Age int) (person, error, error) {
+	var p person = person {
+		age: Age,
+		name: Name,
+		gender: Gender,
+	}
+	var err1 = p.SetAge(Age)
+	var err2 = p.SetGender(Gender)
+	return p, err1, err2 
+}
+
+func (p person) KnowGender() {
+	fmt.Printf("%s's gender - %s.\n", p.name, p.gender)
+}
+
+func (p person) GoToWalk(time int) (string, error) {
+	if time <= 12 && time >= 0 {
+		return ("At " + fmt.Sprint(time) + " o'clock " + p.name + "goes for a walk.\n"), nil
+	}
+	return " ", fmt.Errorf("time can't be more than 12") 
+}
