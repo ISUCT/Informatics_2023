38ee6866c479fe33e831cf046370be0a237cee59
kli133 <kli133@mail.ru>
2023-12-06 18:36:42 +0300

    lab4

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/internal/lab4"
+)
 
 func main() {
-	fmt.Println("Дорошин Евгений Владимирович")
+	fmt.Println("Task A")
+	answersA := lab4.TaskA(1.25, 3.25, 0.4)
+	fmt.Println(answersA)
+	fmt.Println("\nTask B")
+	answersB := lab4.TaskB([5]float64{1.84, 2.71, 3.81, 4.56, 5.62})
+	fmt.Println(answersB)
 }
