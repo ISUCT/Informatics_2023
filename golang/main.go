0d8b9b622c8d110b6ea6d72770c7648cf2d5f304
Kurushina Elizaveta <elisa16384@gmail.com>
2023-12-07 13:11:26 +0300

    labs3,4

--- golang/main.go
+++ golang/main.go
@@ -2,28 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal"
 )
+
 
 func main() {
 	//Лабораторная работа 2
 	fmt.Println("Курушина Елизавета Андреевна")
 
-	//Лабораторная работа 3
-	a := 0.05
-	b := 0.06
-	x_start := 0.2
-	x_end := 0.95
-	x_delta := 0.15
-	fmt.Println("Значение функции №10 при данных задачи A")
-	for x := x_start; x <= x_end; x = x + x_delta {
-		var y1 = math.Acos(math.Pow(x, 2)-math.Pow(b, 2)) / math.Asin(math.Pow(x, 2)-math.Pow(a, 2))
-		fmt.Println(y1)
-	}
-	var h = [5]float64{0.15, 0.26, 0.37, 0.48, 0.56}
-	fmt.Println("Значение функции №10 при данных задачи B")
-	for i := 0; i < 5; i++ {
-		var y2 = math.Acos(math.Pow(h[i], 2)-math.Pow(b, 2)) / math.Asin(math.Pow(h[i], 2)-math.Pow(a, 2))
-		fmt.Println(y2)
-	}
+	//Лабораторне работы 3 и 4
+	fmt.Println("Значение функции №3 при данных задачи А =", internal.TaskA(0.2, 0.95, 0.15))
+	fmt.Println("Значение функции №3 при данных задачи B =", internal.TaskB([]float64{0.15, 0.26, 0.37, 0.48, 0.56}))
 }
