76c29d141125ce185f1d00ef7ab2d89b3f622201
DasaSemenova <Iva-1403@yandex.ru>
2023-12-09 19:00:48 +0300

    laba 4

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -4,24 +4,25 @@ import "math"
 
 func Summ(a, b int) int {
 	return a + b
+}
 
-	a := 2.5
-	b := 4.6
-	x_n := 1.1
-	x_k := 3.6
-	x_z := 0.5
-	x1 := 1.2
-	x2 := 1.28
-	x3 := 1.36
-	x4 := 1.46
-	x5 := 2.35
+func Formula(x float64) float64 {
+	return (math.Pow((2.5 + 4.6*x), 2.5)) / (1 + (math.Log10(2.5 + 4.6*x)))
+}
 
-	for x := x_n; x <= x_k; x = x + x_z 
-		var y1 = (math.Pow((a + b*x), 2.5)) / (1 + (math.Log10(a + b*x)))
+func TaskA(x_n, x_k, x_z float64) []float64 {
+	var num_of_elements int = int((x_k) - (x_n)/x_z + 1) 
+	var otvetA = make([]float64, 0, num_of_elements)
+	fot i := x_n; i <= x_end; i= i + x_z {
+		otvetA = append(otvetA, Formula(i))
+	}
+	return otvetA
+}
 
-
-		var argument = [5]float64{x1, x2, x3, x4, x5} // Массив значений x1-5
-
-	
-		for i := 0; i < 5; i++ 
-			var y2 = (math.Pow((a + b*x), 2.5)) / (1 + (math.Log10(a + b*x)))
+func TaskB(slice []float64) []float64 {
+	var otvetB = make([]float64, 0, len(slice))
+	for _, i := range slice {
+		otvetB = append(otvetB, Formula(i))
+	}
+	return otvetB
+}
