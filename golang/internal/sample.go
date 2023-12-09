aa80884d7fcd898c543dd8d4e3de161beff63ad3
mutil2 <146696499+mutil2@users.noreply.github.com>
2023-12-09 20:06:25 +0300

    sample.go

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -5,12 +5,12 @@ import (
 	"math"
 )
 
-func main() {
+func Main() {
 	zadachkamathForRange()
 	zadachkamathForValues()
 }
 
-func zadachkamathForRange() {
+func ZadachkamathForRange() {
 	a := 4.1
 	b := 2.7
 	xn := 1.2
@@ -18,23 +18,23 @@ func zadachkamathForRange() {
 	deltaX := 0.8
 
 	for x := xn; x <= xk; x += deltaX {
-		result := calculateResult(a, b, x)
+		result := CalculateResult(a, b, x)
 		fmt.Println(result)
 	}
 }
 
-func zadachkamathForValues() {
+func ZadachkamathForValues() {
 	a := 4.1
 	b := 2.7
 	values := []float64{1.9, 2.15, 2.34, 2.73, 3.16}
 
 	for i, x := range values {
-		result := calculateResult(a, b, x)
+		result := CalculateResult(a, b, x)
 		fmt.Printf("Значение функции для x%d=%f: y%d=%f\n", i+1, x, i+1, result)
 	}
 }
 
-func calculateResult(a, b, x float64) float64 {
+func CalculateResult(a, b, x float64) float64 {
 	chisl := a*math.Sqrt(x) - b*math.Log(x)/math.Log(5)
 	znam := math.Log(math.Abs(x-1)) / math.Log(10)
 
