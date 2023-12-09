c0e49ec6d6d384222a957e886fe8ca02139ff63c
IlyaParamonov280 <145789145+IlyaParamonov280@users.noreply.github.com>
2023-12-09 10:53:18 +0300

    Lab 4 main fix

--- golang/main.go
+++ golang/main.go
@@ -9,8 +9,11 @@ import (
 func main() {
 	fmt.Println("Парамонов Илья Сергеевич 1/280")
 	fmt.Println("16 вариант")
+	a := 2.0
+	b := 1.1
 	fmt.Println("Задача А")
-	fmt.Println(internal.TaskA(0.08, 1.08, 0.2))
+	fmt.Println(internal.TaskA(0.08, 1.08, 0.2, a, b))
 	fmt.Println("Задача B")
-	fmt.Println(internal.TaskB([]float64{0.1, 0.3, 0.4, 0.45, 0.65}))
+	slice := []float64{0.1, 0.3, 0.4, 0.45, 0.65}
+	fmt.Println(internal.TaskB(slice, a, b))
 }
