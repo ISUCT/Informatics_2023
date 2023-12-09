9a35feeb9119ebd3fc8a3513c78672004d663bb2
UMBRELLA909 <mix.zhuckow2018@yandex.ru>
2023-12-09 15:00:07 +0300

    lab3correctsize

--- golang/internal/lab4/lab4.go
+++ golang/internal/lab4/lab4.go
@@ -7,7 +7,8 @@ func TaskA(a float64, b float64, x float64) float64 {
 }
 
 func Task1Slice(a float64, b float64, xn float64, xk float64, deltaX float64) []float64 {
-	sliceForTask1 := make([]float64, 0)
+	size := int((xk-xn)/deltaX) + 1
+	sliceForTask1 := make([]float64, 0, size)
 	for x := xn; x <= xk; x += deltaX {
 		sliceForTask1 = append(sliceForTask1, TaskA(a, b, x))
 	}
@@ -17,7 +18,7 @@ func Task1Slice(a float64, b float64, xn float64, xk float64, deltaX float64) []
 func TaskB(slice []float64, a float64, b float64) []float64 {
 	result := make([]float64, len(slice))
 	for i, x := range slice {
-		result[i] = (math.Log10(math.Pow(x, 3)-1) / (math.Log2(a*math.Pow(x, 2)-b) / math.Log2(5)))
+		result[i] = TaskA(a, b, x)
 	}
 	return result
 }
