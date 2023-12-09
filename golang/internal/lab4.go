7d7223244a29421d946b6f8ac7ef2ff23c2c5276
mutil2 <146696499+mutil2@users.noreply.github.com>
2023-12-09 18:03:32 +0300

    lab4.go

--- golang/internal/lab4.go
+++ golang/internal/lab4.go
@@ -18,7 +18,8 @@ func zadachkamathForRange() {
 	deltaX := 0.8
 
 	for x := xn; x <= xk; x += deltaX {
-		calculateAndPrintResult(a, b, x)
+		result := calculateResult(a, b, x)
+		fmt.Println(result)
 	}
 }
 
@@ -28,23 +29,18 @@ func zadachkamathForValues() {
 	values := []float64{1.9, 2.15, 2.34, 2.73, 3.16}
 
 	for i, x := range values {
-		calculateAndPrintResult(a, b, x, i+1)
+		result := calculateResult(a, b, x)
+		fmt.Printf("Значение функции для x%d=%f: y%d=%f\n", i+1, x, i+1, result)
 	}
 }
 
-func calculateAndPrintResult(a, b, x float64, index ...int) {
+func calculateResult(a, b, x float64) float64 {
 	chisl := a*math.Sqrt(x) - b*math.Log(x)/math.Log(5)
-	znam := math.Log(math.Abs(x-1))/math.Log(10)
+	znam := math.Log(math.Abs(x-1)) / math.Log(10)
 
 	if znam == 0 {
-		fmt.Printf("При x=%f знаменатель равен нулю, деление на ноль невозможно.\n", x)
+		return 0
 	} else {
-		var message string
-		if len(index) > 0 {
-			message = fmt.Sprintf("Значение функции для x%d=%f: y%d=%f\n", index[0], x, index[0], chisl/znam)
-		} else {
-			message = fmt.Sprintf("При x=%f: y=%f\n", x, chisl/znam)
-		}
-		fmt.Print(message)
+		return chisl / znam
 	}
 }
