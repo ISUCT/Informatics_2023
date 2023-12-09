22ba70c66ecf93a40c3f007d0a47d7ac721f3d02
deathlesshope <entertohacking@gmail.com>
2023-12-09 17:19:09 +0300

    Grigoryev A. K. 1/278

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -1,5 +1,39 @@
 package internal
 
-func Summ(a, b int) int {
-	return a + b
+import (
+	"fmt"
+	"math"
+)
+
+func Zadacha_A(x float64) []float64 {
+	fmt.Println("Задача A:")
+	var resultA []float64
+	for x <= 2.2 {
+		if math.Abs(x) >= 1 {
+			y := math.Pow(1.2, x) - math.Pow(x, 1.2)
+			resultA = append(resultA, y)
+		}
+		if math.Abs(x) < 1 {
+			y := math.Acos(x)
+			resultA = append(resultA, y)
+		}
+		x += 0.4
+	}
+	return resultA
+}
+
+func Zadacha_B(numbers []float64) []float64 {
+	fmt.Println("Задача B:")
+	var resultB []float64
+	for _, z := range numbers {
+		if math.Abs(z) >= 1 {
+			y := math.Pow(1.2, z) - math.Pow(z, 1.2)
+			resultB = append(resultB, y)
+		}
+		if math.Abs(z) < 1 {
+			y := math.Acos(z)
+			resultB = append(resultB, y)
+		}
+	}
+	return resultB
 }
--- golang/main.go
+++ golang/main.go
@@ -2,49 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"isuct.ru/informatics2022/internal"
 )
 
 func main() {
-	funcs := []func(){
-		zadacha_A,
-		zadacha_B,
-	}
-	for _, f := range funcs {
-		f()
-	}
-}
-
-func zadacha_A() {
-	fmt.Println("Задача A:")
-	x := 0.2
-	for x == x {
-		if math.Abs(x) >= 1 {
-			y := math.Pow(1.2, x) - math.Pow(x, 1.2)
-			fmt.Println(y)
-		}
-		if math.Abs(x) < 1 {
-			y := math.Acos(x)
-			fmt.Println(y)
-		}
-		if x > 2.2 {
-			break
-		}
-		x = x + 0.4
-	}
-}
-
-func zadacha_B() {
-	numbers := [5]float64{0.1, 0.9, 1.2, 1.5, 2.3}
-	fmt.Println("Задача B:")
-	for _, z := range numbers {
-		if math.Abs(z) >= 1 {
-			y := math.Pow(1.2, z) - math.Pow(z, 1.2)
-			fmt.Println(y)
-		}
-		if math.Abs(z) < 1 {
-			y := math.Acos(z)
-			fmt.Println(y)
-		}
-	}
+	fmt.Println(internal.Zadacha_A(0.2))
+	fmt.Println(internal.Zadacha_B([]float64{0.1, 0.9, 1.2, 1.5, 2.3}))
 }
