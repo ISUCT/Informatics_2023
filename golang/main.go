758a2d5a50e2623d2a0755bcad912e465d4ebf9c
mutil2 <146696499+mutil2@users.noreply.github.com>
2023-12-09 20:07:02 +0300

    main.go

--- golang/main.go
+++ golang/main.go
@@ -8,9 +8,9 @@ import (
 func main() {
 	fmt.Println("Мутовкин Илья Алексеевич")
 	
-	taskAResults := lab4.zadachkamathForRange()
+	taskAResults := lab4.ZadachkamathForRange()
 	fmt.Println("Результаты TaskA:", taskAResults)
 
-	taskBResults := lab4.zadachkamathForValues()
+	taskBResults := lab4.ZadachkamathForValues()
 	fmt.Println("Результаты TaskB:", taskBResults)
 }
