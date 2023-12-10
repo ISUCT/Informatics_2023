8abf82a8ac82e9e01dd5eec3b535133be46a2dd8
MarkVRKS <143934528+MarkVRKS@users.noreply.github.com>
2023-12-10 16:19:07 +0300

    Update cat1.go

--- golang/internal/lab5/cat1.go
+++ golang/internal/lab5/cat1.go
@@ -1,1 +1,28 @@
+package main
 
+import "fmt"
+
+type cat struct {
+	name string
+	age  int
+  colour string
+}
+
+func addname() int {
+	return cat.name
+}
+
+func main() {
+	var cat1 = cat{name: "Барсик", age: 3}
+
+	// 1 кошачий год ~ 15 человеческим годам жизни
+
+	vozrast := cat1.age * 15
+	fmt.Println(cat1.name, cat1.age, vozrast)
+
+	cat1.name = "Пушок"
+	cat1.age = 5
+	fmt.Println("Имя кота:", cat1.name, ".", "Возраст кота:", cat1.age, ",", "по переводу на человеческий:", vozrast)
+	//Последний вариант, для более конкрентного обозначения переменных
+
+}
