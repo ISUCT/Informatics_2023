e55365bc54bdada0530f45b7449cb0261341fe5f
m0onblvck <s0ldat0vmail1@gmail.com>
2023-12-10 17:36:58 +0300

    Fixed third lab

--- golang/main.go
+++ golang/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"fmt"
 	"math"
+
+	lab4 "isuct.ru/informatics2022/laba4"
 )
+
+func formula(a, x float64) float64 {
+	return ((math.Pow((math.Log(a + x)), 2)) / (math.Pow((a + x), 2)))
+}
 
 func main() {
 	fmt.Println("Солдатов Иван Сергеевич")
@@ -11,16 +17,15 @@ func main() {
 	a := 2.0
 	x_end := 4.2
 	x_step := 0.6
-	for x_start := 1.2; x_start <= x_end; x_start += x_step {
-		fmt.Println(formula(a, x_start, x_end, x_step))
+	x_start := 1.2
+	for x := x_start; x <= x_end; x += x_step {
+		fmt.Println(formula(a, x))
 	}
 	fmt.Println("------------------------Second_Exercise-------------------------------")
 	array := []float64{1.16, 1.32, 1.47, 1.65, 1.93}
-	for _, x_start := range array {
-		fmt.Println(formula(a, x_start, x_end, x_step))
+	for _, x := range array {
+		fmt.Println(formula(a, x))
 	}
-}
-
-func formula(a, x_start, x_end, x_step float64) float64 {
-	return ((math.Pow((math.Log(a + x_start)), 2)) / (math.Pow((a + x_start), 2)))
+	fmt.Println("------------------------Lab_4-------------------------------")
+	lab4.Solve(a, x_start, x_end, x_step)
 }
