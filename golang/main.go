d40a3315ebbcbe7764190cf282daad5f6ccc3cf8
MarkVRKS <143934528+MarkVRKS@users.noreply.github.com>
2023-12-10 16:03:58 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
-       +"golang/internal/sample.go"
+import (
+	"fmt"
+	"isuct.ru/informatics2022/internal"
+)
 
 func main() {
 	fmt.Println("Вераксо Марк Владимирович 1/280")
-	fmt.Println(internal.fnk1)
-	fmt.Println(internal.fnk2)
+	fmt.Println("1 Вариант")
+	fmt.Println(internal.Cycle(1.28, 3.28, 0.4))
+	fmt.Println(internal.Srez([]float64{1.1, 2.4, 3.6, 1.7, 3.9}))
 	
 }
