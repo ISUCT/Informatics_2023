6b403d0f01d544d6a6ada292beaf72b4c252d64e
mutil2 <146696499+mutil2@users.noreply.github.com>
2023-12-14 11:57:07 +0300

    lab4.go

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -5,29 +5,35 @@ import (
 	"math"
 )
 
-func ZadachkamathForRange() {
+func ZadachkamathForRange() []float64 {
 	a := 4.1
 	b := 2.7
 	xn := 1.2
 	xk := 5.2
 	deltaX := 0.8
 
+	results := make([]float64, 0)
 	for x := xn; x <= xk; x += deltaX {
 		result := CalculateResult(a, b, x)
-		fmt.Println(result)
+		results = append(results, result)
 	}
+	return results
 }
 
-func ZadachkamathForValues() {
+func ZadachkamathForValues() []float64 {
 	a := 4.1
 	b := 2.7
 	values := []float64{1.9, 2.15, 2.34, 2.73, 3.16}
 
+	results := make([]float64, 0)
 	for i, x := range values {
 		result := CalculateResult(a, b, x)
 		fmt.Printf("Значение функции для x%d=%f: y%d=%f\n", i+1, x, i+1, result)
+		results = append(results, result)
 	}
+	return results
 }
+
 
 func CalculateResult(a, b, x float64) float64 {
 	chisl := a*math.Sqrt(x) - b*math.Log(x)/math.Log(5)
