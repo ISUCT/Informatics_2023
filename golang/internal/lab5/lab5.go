4acea1e228806cd30e0af4ebd5b8e367180247e8
DenisSysuykin <tv.denis132@mail.ru>
2023-12-14 20:49:48 +0300

    lab5 change

--- golang/internal/lab5/lab5.go
+++ golang/internal/lab5/lab5.go
@@ -13,9 +13,8 @@ type Cat struct {
 }
 
 func NewCat(age int, weight float64, height float64, name string) (*Cat, error) {
-	switch {
-	case age < 0 || age > 200 || weight < 0 || weight > 200 || height < 0 || height > 200:
-		fmt.Println("error")
+	if age < 0 || age > 200 || weight < 0 || weight > 200 || height < 0 || height > 200 {
+		return nil, errors.New("некорректные данные")
 	}
 
 	return &Cat{
@@ -32,7 +31,7 @@ func (c *Cat) GetAge() int {
 
 func (c *Cat) SetAge(age int) error {
 	if age < 0 || age > 200 {
-		return errors.New("Некорректные данные")
+		return errors.New("некорректные данные")
 	}
 
 	c.age = age
@@ -45,7 +44,7 @@ func (c *Cat) GetWeight() float64 {
 
 func (c *Cat) SetWeight(weight float64) error {
 	if weight < 0 || weight > 200 {
-		return errors.New("Некорректные данные")
+		return errors.New("некорректные данные")
 	}
 
 	c.weight = weight
@@ -58,7 +57,7 @@ func (c *Cat) GetHeight() float64 {
 
 func (c *Cat) SetHeight(height float64) error {
 	if height < 0 || height > 200 {
-		return errors.New("Некорректные данные")
+		return errors.New("некорректные данные")
 	}
 
 	c.height = height
@@ -75,7 +74,7 @@ func (c *Cat) SetName(name string) error {
 }
 
 func main() {
-	cat, err := NewCat(-1, 3.5, 25.6, "Stesha")
+	cat, err := NewCat(5, 3.5, 25.6, "Stesha")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -83,6 +82,6 @@ func main() {
 
 	fmt.Printf("Возраст: %d\n", cat.GetAge())
 	fmt.Printf("Вес: %.2f\n", cat.GetWeight())
-	fmt.Printf("Рост: %.2f\n", cat.GetHeight())
-	fmt.Printf("Имя: %s\n", cat.GetName())
+	fmt.Printf("Рост: %.3f\n", cat.GetHeight())
+	fmt.Println("Имя:", cat.GetName())
 }
