28a61e6ad698780e864b995102111193110ac461
busiginandrew <busigin37@mail.ru>
2023-12-15 19:47:24 +0300

    lab4fix

--- golang/internal/laba4/laba4.go
+++ golang/internal/laba4/laba4.go
@@ -8,7 +8,7 @@ func Calc(x float64) float64 {
 	return math.Pow(math.Abs(math.Pow(x, 2)-2.5), 1/4) + math.Pow(math.Log10(math.Pow(x, 2)), 1/3)
 }
 func TaskA(xn float64, xk float64, deltax float64) []float64 {
-	var answersy []float64 = make([]float64, 0)
+	var answersy []float64 = make([]float64, 0, int(((xk-xn)/deltax))+1)
 	for x := xn; x <= xk; x += deltax {
 		y := Calc(x)
 		answersy = append(answersy, y)
@@ -17,7 +17,7 @@ func TaskA(xn float64, xk float64, deltax float64) []float64 {
 }
 
 func TaskB(znX []float64) []float64 {
-	var answersy []float64 = make([]float64, 0)
+	var answersy []float64 = make([]float64, 0, len(znX))
 	for i := 0; i < len(znX); i++ {
 		x := znX[i]
 		y := Calc(x)
