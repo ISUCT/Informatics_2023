6c021db5bffa4df6f0bf88d306bfe1d2ec5ebfb1
MarkVRKS <143934528+MarkVRKS@users.noreply.github.com>
2023-12-16 18:34:13 +0300

    Update sample.go

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -6,23 +6,24 @@ import (
 
 const b = 2.5
 
-func fnk1(b, c float64) float64 {
-	result := 1 + math.Sin(b*b*b+c*c*c)*math.Sin(b*b*b+c*c*c)/math.Cbrt(b*b*b+c*c*c)
+func fnk1(b, x float64) float64 {
+	result := 1 + math.Sin(b*b*b+x*x*x)*math.Sin(b*b*b+x*x*x)/math.Cbrt(b*b*b+x*x*x)
 	return result
 }
 
 func Cycle(xn, xk, xs float64) []float64 {
 	var valuefnk  = make([]float64, 0, int((xk-xn)/xs) +1)
 	for x := xn; x <= xk; x += xs {
-		valuefnk = append(valuefnk, fnk1(x))
+		valuefnk = append(valuefnk, fnk1(b, x))
 	}
 	return valuefnk
 }	
 
-func Srez(nums []float64) []float64{
+func Srez(b float64, nums []float64) []float64{
 	var nums_of_srez = make([]float64, 0, len(nums))
-	for _, value := range nums {
-		nums_of_srez = append(nums_of_srez, fnk1(value))
+	for _, x := range nums {
+		nums_of_srez = append(nums_of_srez, fnk1(x, b))
 	}
 	return nums_of_srez
 }
+
