45902e51c1a6a06d12866f70ea4146ec0efa4244
User of DK310 <dk310@it.isuct.ru>
2023-12-16 11:40:11 +0300

    Fix linter

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -3,13 +3,12 @@ package internal
 import "math"
 
 func CalculatePoint(x, a, b float64) float64 {
-	return  (math.Pow((a + b*x), 2.5)) / (1 + (math.Log10(a + b*x)))
+	return (math.Pow((a + b*x), 2.5)) / (1 + (math.Log10(a + b*x)))
 }
 
 func CalculateFuncWithStep(a, b, x_n, x_k, x_z float64) []float64 {
-	
-	results := make([]float64, 0, int((x_k - x_n) / x_z) + 1)
-	
+	results := make([]float64, 0, int((x_k-x_n)/x_z)+1)
+
 	for x := x_n; x <= x_k; x += x_z {
 		intermediateResult := CalculatePoint(x, a, b)
 		results = append(results, intermediateResult)
@@ -27,4 +26,3 @@ func CalculateFuncWithArguments(a, b float64, xes []float64) []float64 {
 
 	return results
 }
-
--- golang/main.go
+++ golang/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+
 	"isuct.ru/informatics2022/internal"
 )
 
 func main() {
 	fmt.Println("Петров Леонид Алексеевич")
 	fmt.Println(internal.CalculatePoint(1.1, 3.6, 0.5))
-	fmt.Println(internal.CalculateFuncWithArguments(2.5, 4.6, []float64 {1.2, 1.28, 1.36, 1.46, 2.35}))
-}	
+	fmt.Println(internal.CalculateFuncWithArguments(2.5, 4.6, []float64{1.2, 1.28, 1.36, 1.46, 2.35}))
+}
