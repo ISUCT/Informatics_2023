abdda46ff6e639ead08fa99b21ce821c4c23042f
MarbleTheFox <dviser2005@gmail.com>
2023-12-18 22:29:22 +0300

    edited lab4

--- golang/internal/lab4/lab4.go
+++ golang/internal/lab4/lab4.go
@@ -5,17 +5,19 @@ import (
 )
 
 func Assignment_1(a float64, b float64, x1 float64, x2 float64, deltax float64) []float64 {
-	var res = []float64{}
-	for i := x1; i < x2; i += deltax {
+	n := int((x2-x1)/deltax) + 1
+	res := make([]float64, n)
+	index := 0
+	for i := x1; i <= x2; i += deltax {
 		x := i
 		y := (math.Pow((x-a), (2.00/3.00)) + math.Pow(math.Abs(x+b), (1.00/5.00))) / math.Pow((math.Pow(x, 2)-math.Pow((a+b), 2)), (1.00/9.00))
-		res = append(res, y)
+		res[index] = y
+		index++
 	}
 	return res
 }
 
-func Assignment_2(a float64, b float64, x1 float64, x2 float64, x3 float64, x4 float64, x5 float64) []float64 {
-	var xs = []float64{x1, x2, x3, x4, x5}
+func Assignment_2(a float64, b float64, xs []float64) []float64 {
 	var res = []float64{}
 	for i := 0; i < len(xs); i++ {
 		x := xs[i]
--- golang/main.go
+++ golang/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	result1 := lab4.Assignment_1(0.8, 0.4, 1.23, 7.23, 1.2)
-	result2 := lab4.Assignment_2(0.8, 0.4, 1.88, 2.26, 3.84, 4.55, -6.21)
+	result2 := lab4.Assignment_2(0.8, 0.4, []float64{1.88, 2.26, 3.84, 4.55, -6.21})
 	fmt.Println(result1)
 	fmt.Println(result2)
 }
