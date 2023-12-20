2ad721a64508f18f5d1569fdc2d3603879f19f04
Selaip <skw_93nk@mail.ru>
2023-12-20 17:36:15 +0300

    Lab 3(fix)+Lab 4

--- golang/main.go
+++ golang/main.go
@@ -2,42 +2,20 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	Lab4 "isuct.ru/informatics2022/internal"
 )
-
-var a float64 = 2.0
-var deltaX float64 = 0.6
-
-var x1 float64 = 1.16
-var x2 float64 = 1.32
-var x3 float64 = 1.47
-var x4 float64 = 1.65
-var x5 float64 = 1.93
-
-func work1(xn float64, xk float64) {
-	for {
-		var x float64 = xn
-		var y float64 = (math.Pow(math.Log10(a+x), 2)) / (math.Pow((a + x), 2))
-		fmt.Println("ResultsOfWork1:" + fmt.Sprint(y))
-		xn = xn + deltaX
-		if xn >= xk {
-			break
-		}
-	}
-}
-
-func work2() {
-	Met := [5]float64{x1, x2, x3, x4, x5}
-	fmt.Println(Met)
-	for _, xp := range Met {
-		x := xp
-		var y float64 = (math.Pow(math.Log10(a+x), 2)) / (math.Pow((a + x), 2))
-		fmt.Println("ResultsOfWork2:" + fmt.Sprint(y))
-	}
-}
 
 func main() {
 	fmt.Println("Рыбников Юрий Анатольевич")
-	work1(1.2, 4.2)
-	work2()
+	fmt.Println("20 лет")
+	//Work1
+	fmt.Println("AnswerOnWork1:")
+	resultWork1 := Lab4.Work1(2.0, 0, 1.2, 4.2, 0.6)
+	fmt.Println(resultWork1)
+	//Work2
+	SliceWork2 := []float64{1.16, 1.32, 1.47, 1.65, 1.93}
+	fmt.Println("AnswerOnWork2:")
+	resultSlice := Lab4.Work2(SliceWork2, 2.0, 0)
+	fmt.Println(resultSlice)
 }
