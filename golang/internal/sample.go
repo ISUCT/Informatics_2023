205a7190b73b4e0e70facaa8f24ad1a68424b3a1
DasaSemenova <Iva-1403@yandex.ru>
2023-12-22 23:27:43 +0300

    lab4

--- golang/internal/sample.go
+++ golang/internal/sample.go
@@ -1,5 +1,28 @@
 package internal
 
+import "math"
+
 func Summ(a, b int) int {
 	return a + b
+}
+
+func Formula(a, b, x float64) float64 {
+	return (math.Pow((a + b*x), 2.5)) / (1 + (math.Log10(a + b*x)))
+}
+
+func TaskA(x_n, x_k, x_z, a, b float64) []float64 {
+	var num_of_elements int = int((x_k) - (x_n)/x_z + 1)
+	var otvetA = make([]float64, 0, num_of_elements)
+	for i := x_n; i <= x_k; i += x_z {
+		otvetA = append(otvetA, Formula(a, b, i))
+	}
+	return otvetA
+}
+
+func TaskB(slice []float64, a, b float64) []float64 {
+	var otvetB = make([]float64, 0, len(slice))
+	for i := range slice {
+		otvetB = append(otvetB, Formula(a, b, slice[i]))
+	}
+	return otvetB
 }
--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	a := 2.5
+	b := 4.6
+	x_n := 1.1
+	x_k := 3.6
+	x_z := 0.5
+
+	fmt.Println("Значение функции №18 при данных задачи А =", internal.TaskA(x_n, x_k, x_z, a, b))
+	fmt.Println("Значение функции №18 при данных задачи Б =", internal.TaskB([]float64{1.2, 1.28, 1.36, 1.46, 2.35}, a, b))
 }
+
