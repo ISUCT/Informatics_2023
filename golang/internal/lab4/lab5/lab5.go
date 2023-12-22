a07afacc8e66375186c0a29196016a821df6f37c
UMBRELLA909 <mix.zhuckow2018@yandex.ru>
2023-12-22 19:14:35 +0300

    lab5fix

--- golang/internal/lab4/lab5/lab5.go
+++ golang/internal/lab4/lab5/lab5.go
@@ -4,6 +4,12 @@ import (
 	"errors"
 )
 
+const (
+	ErrInvalidWeight = "неверно указан вес, пожалуйста, укажите реальное значение"
+	ErrInvalidHeight = "неверно указан рост, пожалуйста, укажите реальное значение"
+	ErrInvalidAge    = "неверно указан возраст, пожалуйста, укажите реальное значение"
+)
+
 type Person struct {
 	weight float64
 	height float64
@@ -11,15 +17,21 @@ type Person struct {
 	name   string
 }
 
-func NewPerson(weight float64, height float64, age int, name string) (*Person, error) {
-	if weight <= 0 || weight > 1000 || height <= 0 || height > 500 || age < 0 || age > 200 {
-		return nil, errors.New("неверно указаны значения, пожалуйста, укажите реальные значения")
-	}
+func NewPerson(weight, height float64, age int, name string) (*Person, error) {
 	p := &Person{
 		weight: weight,
 		height: height,
 		age:    age,
 		name:   name,
+	}
+	if err := p.SetWeight(weight); err != nil {
+		return nil, err
+	}
+	if err := p.SetHeight(height); err != nil {
+		return nil, err
+	}
+	if err := p.SetAge(age); err != nil {
+		return nil, err
 	}
 	return p, nil
 }
@@ -29,8 +41,8 @@ func (p *Person) GetWeight() float64 {
 }
 
 func (p *Person) SetWeight(weight float64) error {
-	if weight < 0 || weight > 200 {
-		return errors.New("неверно указан вес, пожалуйста, укажите реальное значение")
+	if weight < 0 || weight > 1000 {
+		return errors.New(ErrInvalidWeight)
 	}
 	p.weight = weight
 	return nil
@@ -41,8 +53,8 @@ func (p *Person) GetHeight() float64 {
 }
 
 func (p *Person) SetHeight(height float64) error {
-	if height < 0 || height > 200 {
-		return errors.New("неверно указан рост, пожалуйста, укажите реальное значение")
+	if height < 0 || height > 500 {
+		return errors.New(ErrInvalidHeight)
 	}
 	p.height = height
 	return nil
@@ -54,7 +66,7 @@ func (p *Person) GetAge() int {
 
 func (p *Person) SetAge(age int) error {
 	if age < 0 || age > 200 {
-		return errors.New("неверно указан возраст, пожалуйста, укажите реальное значение")
+		return errors.New(ErrInvalidAge)
 	}
 	p.age = age
 	return nil
