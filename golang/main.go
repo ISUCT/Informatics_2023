e910fdfa00ca42c9505c73de46d0bd5bca8f60fb
MarkVRKS <143934528+MarkVRKS@users.noreply.github.com>
2023-12-23 12:54:34 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"fmt"
 	"isuct.ru/informatics2022/internal"
+
 )
 
 func main() {
@@ -13,7 +14,35 @@ func main() {
 	fmt.Println("1 Вариант")
 	fmt.Println(answer1)
 	fmt.Println(answer2)
+
+	firstcat, error := internal.NewCat("Pushok", 5, "Black")
+	checkForError(error)
+	secondcat, error := internal.NewCat("Aegis", 7, "Yellow")
+	checkForError(error)
+	thirdcat, error :=internal.NewCat("Simba", 10, "White")
+
+	firstcat.AddAge(9)
+	checkForError(error)
+
+	secondcat.AddName("Aegis")
+	checkForError(error)
+
+	thirdcat.AddColour("Gray")
+	checkForError(error)
+
+	fmt.Println("Первому коту мы обновили возраст на значение:", firstcat.AddAge(9))
+	fmt.Println("Второму коту мы добавили новое имя:", firstcat.AddName("Aegis"))
+	fmt.Println("Третьему коту мы добавили новый цвет:", firstcat.AddColour("Gray"))
 	
+	fmt.Println(firstcat)
+	fmt.Println(secondcat)
+	fmt.Println(thirdcat)
+
+
+
+//Первый пример: первому коту мы добавили новый возраст - 9
+//Второй пример: второму коту мы добавили новое имя - Aegis
+//Третий пример: третьему коту мы добавили новый цвет - Gray
 }
 
 func checkForError(err error) {
