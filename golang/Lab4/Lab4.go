48006f4678779680756221c78c060adbbedf24a8
mutil2 <146696499+mutil2@users.noreply.github.com>
2023-12-24 12:48:14 +0300

    Update Lab4.go

--- golang/Lab4/Lab4.go
+++ golang/Lab4/Lab4.go
@@ -8,9 +8,16 @@ import (
 func Lab4() {
 	fmt.Println("Мутовкин Илья Алексеевич")
 	
-	taskAResults := internal.ZadachkamathForRange()
+	a := 4.1
+	b := 2.7
+	xn := 1.2
+	xk := 5.2
+	deltaX := 0.8
+	values := []float64{1.9, 2.15, 2.34, 2.73, 3.16}
+
+	taskAResults := internal.ZadachkamathForRange(a, b, xn, xk, deltaX)
 	fmt.Println("Результаты TaskA:", taskAResults)
 
-	taskBResults := internal.ZadachkamathForValues()
+	taskBResults := internal.ZadachkamathForValues(a, b, values)
 	fmt.Println("Результаты TaskB:", taskBResults)
 }
