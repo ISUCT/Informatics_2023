b85d7801a835818f127741f424d6110601c492fa
mutil2 <146696499+mutil2@users.noreply.github.com>
2023-12-24 12:31:48 +0300

    main.go

--- golang/main.go
+++ golang/main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"isuct.ru/informatics2022/internal"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Мутовкин Илья Алексеевич")
-	
-	taskAResults := internal.ZadachkamathForRange()
-	fmt.Println("Результаты TaskA:", taskAResults)
-
-	taskBResults := internal.ZadachkamathForValues()
-	fmt.Println("Результаты TaskB:", taskBResults)
+	fmt.Println("Hello world")
 }
