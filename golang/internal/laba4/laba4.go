bda02c29c9acc51e4e4031594e4a764766b57694
MaksimChmirkov <maksimkaron9@gmail.com>
2023-12-25 19:49:24 +0300

    laba 4 fix

--- golang/internal/laba4/laba4.go
+++ golang/internal/laba4/laba4.go
@@ -4,20 +4,23 @@ import (
 	"math"
 )
 
+func calc(a, b, x float64) float64 {
+	return (a*math.Pow(x, 1/3) - b*(math.Log(x)/math.Log(5))) / math.Pow(math.Log10(x-1), 3)
+}
+
 func TaskA(xn float64, xk float64, a1, b1, deltax float64) []float64 {
-	var answers1 = make([]float64, 0, 6)
+	var answers1 = make([]float64, 0, int((xk-xn)/deltax)+1)
 	for x := xn; x <= xk; x += deltax {
-		var y float64 = (a1*math.Pow(x, 1/3) - b1*(math.Log(x)/math.Log(5))) / math.Pow(math.Log10(x-1), 3)
+		y := calc(a1, b1, x)
 		answers1 = append(answers1, y)
 	}
 	return answers1
 }
 
 func TaskB(a2, b2 float64, znachX []float64) []float64 {
-	var answers2 = make([]float64, 0, 5)
-	for i := 0; i < len(znachX); i++ {
-		var x float64 = znachX[i]
-		var y float64 = (a2*math.Pow(x, 1/3) - b2*(math.Log(x)/math.Log(5))) / math.Pow(math.Log10(x-1), 3)
+	var answers2 = make([]float64, 0, len(znachX))
+	for _, x := range znachX {
+		y := calc(a2, b2, x)
 		answers2 = append(answers2, y)
 	}
 	return answers2
