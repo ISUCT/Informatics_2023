d94d36b1507ec6e6d3a8e1b96f80023ed6085490
Alexey_Ivanov <alexcar1801165@gmail.com>
2024-01-19 23:59:26 +0300

    planeStruct#2

--- golang/main.go
+++ golang/main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println("Иванов Алексей Алексеевич 1/278")
 	fmt.Println(internal.SolveA(1.1, 0.09, 1.2, 2.2, 0.2))
 	fmt.Println(internal.SolveB([]float64{4.48, 3.56, 2.78, 5.28, 3.21}))
-
+	// planeStruct Lab5
 	fmt.Println("\n First Plane: \n")
 	plane1, err := plane.NewPlane("Космолёт", "Фиолетовый", 3124)
 	if err != nil {
@@ -25,11 +25,10 @@ func main() {
 	fmt.Println(plane1.GetSpeed())
 	plane1.SetSpeed(3400)
 	fmt.Println(plane1.GetSpeed())
-
 	fmt.Println("\n Second Plane: \n")
 	plane2, err := plane.NewPlane("Boeing 737", "Серый", 400)
 	if err != nil {
-		fmt.Println("%v", err)
+		fmt.Printf("%v", err)
 	}
 	fmt.Println(plane2.GetModel())
 	fmt.Println(plane2.GetColor())
