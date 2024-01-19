ccaa16f2ebd6ac1a4198e848b3418a5f9a4c8591
YarDenKiselev <yaroslavkiselev.2002@gmail.com>
2024-01-19 20:26:54 +0300

    lab4(fixed)+lab5

--- golang/internal/lab4/lab4.go
+++ golang/internal/lab4/lab4.go
@@ -3,18 +3,21 @@ package lab4
 import (
 	"math"
 )
+
 func Task1(a, b, xn, xk, deltax float64) []float64 {
-	var answers1 []float64 = make([]float64, 0)
-	for x := xn; x <= xk; x+= deltax {
-		y := (math.Pow(math.Arccos(math.Pow(x, 2) - math.Pow(b, 2))) / math.Pow(math.Arcsin(math.Pow(x, 2) - math.Pow(a, 2))))
+	var answers1 []float64 = make([]float64, 0, int((xk-xn)/deltax)+1)
+	for x := xn; x <= xk; x += deltax {
+		y := (math.Acos(math.Pow(x, 2) - math.Pow(b, 2))) / (math.Asin(math.Pow(x, 2) - math.Pow(a, 2)))
+		answers1 = append(answers1, y)
 	}
+	return answers1
 }
 
 func Task2(a float64, b float64, znX []float64) []float64 {
-	var answers2 []float64 = make([]float64, 0)
-	for _, value := range znX {
-		x := value
-		y := (math.Pow(math.Arccos(math.Pow(x, 2) - math.Pow(b, 2))) / math.Pow(math.Arcsin(math.Pow(x, 2) - math.Pow(a, 2))))
+	var answers2 []float64 = make([]float64, 0, len(znX))
+	for _, x := range znX {
+		y := (math.Acos(math.Pow(x, 2) - math.Pow(b, 2))) / (math.Asin(math.Pow(x, 2) - math.Pow(a, 2)))
+		answers2 = append(answers2, y)
 	}
-return answers2
+	return answers2
 }
--- golang/main.go
+++ golang/main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"fmt"
+
 	"isuct.ru/informatics2022/internal/lab4"
+	"isuct.ru/informatics2022/internal/lab5"
 )
 
 func main() {
 	fmt.Println("Киселев Ярослав Денисович")
 	fmt.Println("---")
 
-	a1 := Task1(0.05, 0.06, 0.2 0.95, 0.15)
+	a1 := lab4.Task1(0.05, 0.06, 0.2, 0.95, 0.15)
 	fmt.Print(a1)
-	fmt.Println("---")
 
-	a2 := Task2(0.05, 0.06, []float64{0.15, 0.26, 0.37, 0,48, 0.56})
+	a2 := lab4.Task2(0.05, 0.06, []float64{0.15, 0.26, 0.37, 0, 48, 0.56})
 	fmt.Println(a2)
+
+	table, err := lab5.NewTable(100, 130, 50)
+
+	if err == nil {
+		fmt.Printf("Table's height is %d\n", table.GetHeight())
+		fmt.Printf("Table's length is %d\n", table.GetLength())
+		fmt.Printf("Table's width is %d\n", table.GetWidth())
+	} else {
+		fmt.Println(err)
+	}
 }
