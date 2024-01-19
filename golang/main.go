18899b272d70a297127c4ccd5116176422402157
AlinaKaladina <145924518+AlinaKaladina@users.noreply.github.com>
2024-01-19 22:43:19 +0300

    3-4l ab

--- golang/main.go
+++ golang/main.go
@@ -7,8 +7,9 @@ import (
 )
 
 func main() {
+	//Лабораторная 2
 	fmt.Println("Калядина Алина Олеговна")
-	fmt.Println("Вариант 7")
+	//Лабораторная 3-4 (7 Вариант)
 	fmt.Println(internal.CalcA(3.2, 6.2, 0.6))
 	fmt.Println(internal.CalcB([]float64{4.48, 3.56, 2.78, 5.28, 3.21}))
 }
