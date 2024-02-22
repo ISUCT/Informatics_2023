51d83249d1732bf009d499f9be8e5a8487abc76c
AlenaY2312 <152083878+AlenaY2312@users.noreply.github.com>
2024-02-22 12:32:16 +0300

    lab5

--- golang/main.go
+++ golang/main.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 
 	"isuct.ru/informatics2022/internal/laba4"
+	"isuct.ru/informatics2022/internal/laba5"
 )
 
 func main() {
 	fmt.Println("Яковлева Алёна")
+	dog, err := laba5.NewDog(20, 65, "Овчарка")
+	if err == nil {
+		fmt.Printf("Dog age is %d\n", dog.GetAge())
+		fmt.Printf("Dog ves is %d\n", dog.GetVes())
+		fmt.Printf("Dog poroda is %s\n", dog.GetPoroda())
+	}else{
+		fmt.Println(err)
+	}
 	y1 := laba4.Task1(0.22, 0.92, 0.14)
 	y2 := laba4.Task2([]float64{0.1, 0.35, 0.4, 0.55, 0.6})
 	fmt.Println(y1)
